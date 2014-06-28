c919fc4bb03a7da0debba76ac8a2fa918dff476c
Will Bush <will.g.bush@gmail.com>
2014-06-27 19:34:11 -0500

    remove xcape; add key swap method; add script for symlinks

---
+++ init_symlinks.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+ln -s ~/dotfiles/vim ~/.vim
+ln -s ~/dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/gimp-2.8 ~/.gimp-2.8
+ln -s ~/dotfiles/zshrc ~/.zshrc
+ln -s ~/dotfiles/.keyswapper ~/.keyswapper
