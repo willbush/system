96f143b7b1148705b18ffd137210a06137d32df8
Will Bush <will.g.bush@gmail.com>
2015-02-10 23:07:05 -0600

    update sym link init, redirect evilbegins readme to actual repo
    Former-commit-id: e3321fc1802767ea148946b5d8f93992fdac3014

--- init_symlinks.sh
+++ init_symlinks.sh
@@ -4,5 +4,4 @@ ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/dotfiles/emacs/EvilBegins/emacs ~/.emacs
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/dotfiles/gimp-2.8 ~/.gimp-2.8
 ln -s ~/dotfiles/zshrc ~/.zshrc
