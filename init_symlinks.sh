46112929472c9da1da60250ea97d12f06c05d57e
Will Bush <will.g.bush@gmail.com>
2015-07-25 20:23:31 -0500

    removed unused items; add nvim
    Former-commit-id: d0a988a130ae9c527f32df65a69e385d9b843eb8

--- init_symlinks.sh
+++ init_symlinks.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
-ln -s ~/dotfiles/vim ~/.vim
-ln -s ~/dotfiles/vim/vimrc ~/.vimrc
-ln -s ~/dotfiles/emacs/EvilBegins/emacs ~/.emacs
+ln -s ~/dotfiles/nvim ~/.nvim
+ln -s ~/dotfiles/nvim/nvimrc ~/.nvimrc
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/zshrc ~/.zshrc
