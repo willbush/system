d2a48fa756fa28a6c4e93edf266fd95a747346db
Will Bush <will.g.bush@gmail.com>
2018-03-30 01:06:33 -0500

    rename dot files better to have a dot than not because this triggers emacs syntax highlighting
    Former-commit-id: dd15b4d6ca48a83da3e19e430a7f52d016a89523

--- init_symlinks.sh
+++ init_symlinks.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-ln -s ~/dotfiles/spacemacs/spacemacs ~/.spacemacs
-ln -s ~/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/dotfiles/zshrc ~/.zshrc
-ln -s ~/dotfiles/zshenv ~/.zshenv
+ln -s ~/dotfiles/spacemacs/.spacemacs ~/.spacemacs
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.zshenv ~/.zshenv
