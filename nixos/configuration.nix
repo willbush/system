6c235b399f636a4d56fe384cc3d9d117ec4dc44e
Will Bush <will.g.bush@gmail.com>
2018-06-30 02:04:29 -0500

    config updates
    Former-commit-id: c3d86a5540fd7205aeef0fe6020287db927cd32e

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -14,11 +14,14 @@
   time.timeZone = "America/Chicago";
 
   # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
-  boot.loader.grub.useOSProber = true; # autodetect installed OSes
+  boot = {
+    cleanTmpDir = true; # cleans all files in /tmp during boot
+    loader.grub.enable = true;
+    loader.grub.version = 2;
+    # Define on which hard drive you want to install Grub.
+    loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+    loader.grub.useOSProber = true; # autodetect installed OSes
+  };
 
   # Enable sound.
   sound.enable = true;
@@ -35,36 +38,59 @@
     geoclue2.enable = true;
 
     xserver = {
-        # Enable the X11 windowing system.
-        enable = true;
-        layout = "us";
-        xkbOptions = "terminate:ctrl_alt_bksp, caps:swapescape";
+      # Enable the X11 windowing system.
+      enable = true;
+      layout = "us";
+      xkbOptions = "terminate:ctrl_alt_bksp, caps:swapescape";
 
-        windowManager.xmonad = {
+      windowManager.xmonad = {
+          enable = true;
+          enableContribAndExtras = true;
+          extraPackages = haskellPackges: [
+              haskellPackges.xmonad-contrib
+              haskellPackges.xmonad-extras
+              haskellPackges.xmonad
+          ];
+      };
+      windowManager.default = "xmonad";
+      displayManager = {
+        auto = {
             enable = true;
-            enableContribAndExtras = true;
-            extraPackages = haskellPackges: [
-                haskellPackges.xmonad-contrib
-                haskellPackges.xmonad-extras
-                haskellPackges.xmonad
-            ];
+            user = "will";
         };
-        windowManager.default = "xmonad";
-        displayManager = {
-            auto = {
-                enable = true;
-                user = "will";
-            };
-        };
+      };
     };
-};
+  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.extraUsers.will = {
     isNormalUser = true;
     home = "/home/will";
     uid = 1000;
-    extraGroups = [ "wheel" "networkmanager" ];
+    extraGroups = [ "wheel" "networkmanager" "docker"];
+    shell = pkgs.zsh;
+  };
+
+  virtualisation.docker.enable = true;
+
+  programs = {
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      interactiveShellInit = ''
+alias dropbox="docker exec -it dropbox dropbox"
+alias dropbox-start="docker run -d --restart=always --name=dropbox \
+  -v /home/will/Dropbox:/dbox/Dropbox \
+  -v /home/will/.dropbox:/dbox/.dropbox \
+  -e DBOX_UID=1000 -e DBOX_GID=100 janeczku/dropbox"
+'';
+
+      ohMyZsh = {
+        enable = true;
+        plugins = ["git" "vi-mode"];
+        theme = "agnoster";
+      };
+    };
   };
 
   # List packages installed in system profile. To search, run:
@@ -75,13 +101,36 @@
     emacs
     firefox
     git
+    ripgrep
     redshift
     keepassxc
+    docker
+    alacritty
+    fzf
     #synapse
-    #dropbox
     #hexchat
-    #termite
   ];
+
+  fonts = {
+    enableFontDir = true;
+    enableGhostscriptFonts = true;
+    fonts = with pkgs; [
+      anonymousPro
+      hack-font
+      corefonts
+      dejavu_fonts
+      font-droid
+      freefont_ttf
+      google-fonts
+      inconsolata
+      liberation_ttf
+      powerline-fonts
+      source-code-pro
+      terminus_font
+      ttf_bitstream_vera
+      ubuntu_font_family
+    ];
+  };
 
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
