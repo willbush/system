5806ee9510d1073100ad607cf6d8f37e98d38c54
Will Bush <will.g.bush@gmail.com>
2018-06-30 21:44:14 -0500

    add initial xmonad config

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,14 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main = do
+  xmonad $ defaultConfig
+    { manageHook = manageDocks <+> manageHook defaultConfig
+    -- , layoutHook = avoidStruts  $ layoutHook defaultConfig
+    , terminal = "alacritty"
+    , modMask = mod4Mask -- Rebind Mod to the super key
+    }
