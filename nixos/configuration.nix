aa109cbc025f96deb09d6a66eeb613900adf67f9
Will Bush <will.g.bush@gmail.com>
2018-08-09 23:28:33 -0500

    fix warning by updating stateVersion
    Former-commit-id: 15331aa16f8ed669c2659d5195d51ec4df260370

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -174,5 +174,5 @@
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
-  system.nixos.stateVersion = "18.03"; # Did you read the comment?
+  system.stateVersion = "18.03"; # Did you read the comment?
 }
