b0b2d5db4dd850321dc3c71ae42eeda3e6172068
Will Bush <will.g.bush@gmail.com>
2018-12-19 22:21:41 -0600

    add bat script to make symbolic link in windows

---
+++ mklinks.bat
@@ -0,0 +1,3 @@
+mklink C:\Users\Will\.emacs C:\Users\Will\system\emacs\.emacs
+mklink C:\Users\Will\.gitconfig C:\Users\Will\system\.gitconfig
+mklink C:\Users\Will\.vsvimrc C:\Users\Will\system\.vsvimrc
