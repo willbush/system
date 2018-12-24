46df3eb74045c8a1c3e716d63bbd6caa0bbcc941
Will Bush <will.g.bush@gmail.com>
2018-12-24 04:52:29 -0600

    move fonts into their own file
    Former-commit-id: 6d105e1feff30c18d370376f98737e2eb6693c65

---
+++ nixos/fonts.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, ... }:
+
+{
+  fonts = {
+    enableFontDir = true;
+    enableGhostscriptFonts = true;
+    fonts = with pkgs; [
+      hack-font
+      source-code-pro
+      powerline-fonts
+      corefonts
+      dejavu_fonts
+      font-droid
+      freefont_ttf
+      google-fonts
+      inconsolata
+      liberation_ttf
+      terminus_font
+      ttf_bitstream_vera
+      ubuntu_font_family
+    ];
+  };
+}
