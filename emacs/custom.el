bb2b0dcf64f1cb6b24f93b4bd96f8671cb2f19d1
willbush <will.g.bush@gmail.com>
2018-12-28 23:47:12 -0600

    emacs, move custom-* into custom.el

---
+++ emacs/custom.el
@@ -0,0 +1,25 @@
+;; -*- mode: emacs-lisp; lexical-binding: t -*-
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 113 :width normal :foundry "simp" :family "Hack")))))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (doom-vibrant)))
+ '(custom-safe-themes
+   (quote
+    ("1c082c9b84449e54af757bcae23617d11f563fc9f33a832a8a2813c4d7dfb652" default)))
+ '(evil-collection-setup-minibuffer t)
+ '(package-selected-packages
+   (quote
+    (projectile treemacs-icons-dired markdown-toc markdown-mode hindent dante haskell-mode golden-ratio esup omnisharp flyspell-correct-ivy winum nix-mode deadgrep evil-magit magit smex which-key use-package rainbow-delimiters evil-visualstar evil-surround evil-numbers evil-matchit evil-leader evil-exchange doom-themes doom-modeline dashboard counsel company avy)))
+ '(whitespace-style
+   (quote
+    (face tabs spaces trailing space-before-tab newline indentation empty space-after-tab space-mark tab-mark))))
