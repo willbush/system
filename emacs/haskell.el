c6a362bbae44511116c573adb9db663f90ee1ac5
Will Bush <will.g.bush@gmail.com>
2018-12-29 00:38:18 -0600

    emacs, move functions and haskell stuff into their own files

---
+++ emacs/haskell.el
@@ -0,0 +1,22 @@
+;;; -*- lexical-binding: t; -*-
+
+(use-package haskell-mode
+  :mode ("\\.hs\\'" . haskell-mode)
+  :init
+  (add-hook 'haskell-mode-hook
+            (lambda ()
+              (setq-local flycheck-check-syntax-automatically '(save mode-enabled)))))
+
+(use-package dante
+  :after haskell-mode
+  :commands 'dante-mode
+  :init
+  (add-hook 'haskell-mode-hook 'dante-mode)
+  (add-hook 'dante-mode-hook
+     '(lambda () (flycheck-add-next-checker 'haskell-dante
+                  '(warning . haskell-hlint)))))
+
+(use-package hindent
+  :hook (haskell-mode-hook . hindent-mode))
+
+(provide 'haskell)
