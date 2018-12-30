fc8778ebd68aa558f10dca616cf06fe2de7fe2c5
Will Bush <will.g.bush@gmail.com>
2018-12-30 02:18:26 -0600

    fix mklinks bat script
    Former-commit-id: ccd9dcc379c9431b7ad5fbc4c0353f71e6bc479d

--- win/mklinks.bat
+++ win/mklinks.bat
@@ -1,6 +1,6 @@
 set scriptPath=%~dp0
 
-mklink %USERPROFILE%\.emacs.d\ %scriptPath%..\stow\.emacs.d\
+mklink /J %USERPROFILE%\.emacs.d %scriptPath%..\stow\.emacs.d
 mklink %USERPROFILE%\.gitconfig %scriptPath%\config\.gitconfig
 mklink %USERPROFILE%\.vsvimrc %scriptPath%\config\.vsvimrc
 
