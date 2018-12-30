71c09d1f4f70b0d1aedddbdc2d0f28e0ebdfbf07
Will Bush <will.g.bush@gmail.com>
2018-12-30 02:54:17 -0600

    simplify nixos init scripts
    I gold plated this a bit thinking I was going to init all my symbolic links through these scripts, but after I discovered stow, I realized this is silly.

--- nixos/init_user.sh
+++ nixos/init_user.sh
@@ -2,7 +2,4 @@
 
 # ensure this folder exists
 mkdir -pv ~/.config/nixpkgs/
-
-if [[ ! -e ~/.config/nixpkgs/home.nix ]] ; then
-  ln -sv $(pwd)/home.nix ~/.config/nixpkgs/home.nix
-fi
+ln -sv $(pwd)/home.nix ~/.config/nixpkgs/home.nix
