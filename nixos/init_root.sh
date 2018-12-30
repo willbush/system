765087763e07798f4fc34b2af1d81177f6fdcc16
willbush <will.g.bush@gmail.com>
2018-12-30 02:54:17 -0600

    simplify nixos init scripts
    I gold plated this a bit thinking I was going to init all my symbolic links through these scripts, but after I discovered stow, I realized this is silly.

--- nixos/init_root.sh
+++ nixos/init_root.sh
@@ -1,20 +1,4 @@
 #!/usr/bin/env bash
 
-abort () {
-    printf "Aborting! ERROR:\n$1\n"
-    exit 1
-}
+ln -sv $(pwd)/configuration.nix /etc/nixos/configuration.nix
 
-if [[ ! -d /etc/nixos ]]; then
-    msg="No /etc/nixos/ folder found.\n"
-    msg+="This script is meant for NixOS and it typically has that folder."
-    abort "$msg"
-fi
-
-if [[ $(whoami) != "root" ]]; then
-    abort "You must run this script as root.\n"
-fi
-
-if [[ ! -e /etc/nixos/configuration.nix ]] ; then
-  ln -sv $(pwd)/configuration.nix /etc/nixos/configuration.nix
-fi
