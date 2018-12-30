4635800a04f7b75807203ab6be248307867d721a
Will Bush <will.g.bush@gmail.com>
2018-12-29 19:09:52 -0600

    fix mklink paths (not tested yet)

--- win10/mklinks.bat
+++ win10/mklinks.bat
@@ -1,7 +1,7 @@
 set scriptPath=%~dp0
 
-mklink %USERPROFILE%\.emacs %scriptPath%emacs\.emacs
-mklink %USERPROFILE%\.gitconfig %scriptPath%.gitconfig
-mklink %USERPROFILE%\.vsvimrc %scriptPath%.vsvimrc
+mklink %USERPROFILE%\.emacs.d\ %scriptPath%..\.emacs.d\
+mklink %USERPROFILE%\.gitconfig %scriptPath%\config\.gitconfig
+mklink %USERPROFILE%\.vsvimrc %scriptPath%\config\.vsvimrc
 
 pause
