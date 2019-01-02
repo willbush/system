799fd2b68cdb7133cfada0c41830d38e0bcc6252
Will Bush <will.g.bush@gmail.com>
2019-01-02 00:08:18 -0600

    replace evil-leader with general

--- stow/.emacs.d/src/init-org.el
+++ stow/.emacs.d/src/init-org.el
@@ -1,10 +1,5 @@
 ;;; -*- lexical-binding: t; -*-
 
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cc" 'org-capture)
-(global-set-key "\C-cb" 'org-switchb)
-
 (add-hook 'org-mode-hook
           '(lambda ()
              (setq show-trailing-whitespace t)
