9b801900bfa1130b3418907834af684335492b8a
Will Bush <will.g.bush@gmail.com>
2019-01-12 02:07:25 -0600

    set some doom-modeline settings

--- stow/.emacs.d/src/init-themes.el
+++ stow/.emacs.d/src/init-themes.el
@@ -7,6 +7,10 @@
 
 ;; doom modeline requires M-x all-the-icons-install-fonts
 (use-package doom-modeline
-  :hook (after-init . doom-modeline-init))
+  :hook (after-init . doom-modeline-init)
+  :config
+  (setq doom-modeline-icon t
+        doom-modeline-major-mode-color-icon t
+        doom-modeline-buffer-file-name-style 'truncate-upto-root))
 
 (provide 'init-themes)
