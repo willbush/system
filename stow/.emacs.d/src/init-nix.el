855e4d4079c3135633ce6bdff8c540e4d72b1303
willbush <will.g.bush@gmail.com>
2019-01-27 20:21:10 -0600

    emacs, add company-nixos-options

--- stow/.emacs.d/src/init-nix.el
+++ stow/.emacs.d/src/init-nix.el
@@ -1,7 +1,12 @@
 ;;; -*- lexical-binding: t; -*-
 
 (use-package nix-mode
-  :mode "\\.nix\\'")
+  :mode "\\.nix\\'"
+  :config
+  (add-hook 'nix-mode-hook
+              (lambda ()
+                (add-to-list (make-local-variable 'company-backends)
+                             '(company-nixos-options)))))
 
 (use-package nix-sandbox
   :commands (nix-shell-command
@@ -10,5 +15,8 @@
              nix-find-sandbox
              nix-current-sandbox
              nix-executable-find))
+
+(use-package company-nixos-options
+  :commands 'company-nixos-options)
 
 (provide 'init-nix)
