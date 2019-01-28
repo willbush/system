66ab268dd0ee282681bb8fef01b7017f7271da6c
Will Bush <will.g.bush@gmail.com>
2019-01-27 18:54:31 -0600

    emacs, add nix-sandbox

--- stow/.emacs.d/src/init-nix.el
+++ stow/.emacs.d/src/init-nix.el
@@ -3,4 +3,12 @@
 (use-package nix-mode
   :mode "\\.nix\\'")
 
+(use-package nix-sandbox
+  :commands (nix-shell-command
+             nix-shell
+             nix-compile
+             nix-find-sandbox
+             nix-current-sandbox
+             nix-executable-find))
+
 (provide 'init-nix)
