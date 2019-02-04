a621db1fb2c0446af150f824bbf4afcba0bd4f13
Will Bush <will.g.bush@gmail.com>
2019-02-04 00:29:01 -0600

    emacs, changed company settings
    Former-commit-id: 804ada62c4a91f155ce8e5ca16d2b696127189b1

--- stow/.emacs.d/src/init-completion.el
+++ stow/.emacs.d/src/init-completion.el
@@ -35,7 +35,8 @@
 (use-package company
   :hook (after-init . global-company-mode)
   :config
-  (setq company-idle-delay 0.3))
+  (setq company-idle-delay 0.2
+        company-minimum-prefix-length 1))
 
 (use-package projectile
   :config
