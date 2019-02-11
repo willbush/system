38956cc5cef411ca105fca2c91c41a0aba033eee
willbush <will.g.bush@gmail.com>
2019-02-11 14:56:40 -0600

    increase company-minimum-prefix-length to 2
    Opening blocks in org mode by typing <s and <q etc is quite annoying when
    company kicks in and starts trying to complete off of one letter.

--- stow/.emacs.d/src/init-completion.el
+++ stow/.emacs.d/src/init-completion.el
@@ -39,7 +39,7 @@
          ("C-p" . company-select-previous))
   :config
   (setq company-idle-delay 0.2
-        company-minimum-prefix-length 1))
+        company-minimum-prefix-length 2))
 
 (use-package projectile
   :config
