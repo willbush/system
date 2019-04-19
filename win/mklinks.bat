6e1f4e4e40323261b10720011d14f4c73a594b0e
Will Bush <will.g.bush@gmail.com>
2019-04-19 04:11:36 -0500

    fix some paths that still had stow in them
    Former-commit-id: 167f40c96a4fc6f9a28c479b927f17b476af7338

--- win/mklinks.bat
+++ win/mklinks.bat
@@ -1,6 +1,6 @@
 set scriptPath=%~dp0
 
-mklink /J %USERPROFILE%\.emacs.d %scriptPath%..\stow\.emacs.d
+mklink /J %USERPROFILE%\.emacs.d %scriptPath%..\emacs
 mklink %USERPROFILE%\.gitconfig %scriptPath%\config\.gitconfig
 mklink %USERPROFILE%\.vsvimrc %scriptPath%\config\.vsvimrc
 
