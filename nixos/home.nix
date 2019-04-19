7e32360e93b9c58614ad4a34265e4081e2057165
Will Bush <will.g.bush@gmail.com>
2019-04-19 18:44:26 -0500

    use .Xresources to set emacs gui properties
    This is awesome because is saves 250-500 ms on startup time.
    
    
    Former-commit-id: 29f77db9b28069318cd88423e5e1204ae29617d6

--- nixos/home.nix
+++ nixos/home.nix
@@ -33,6 +33,17 @@ in
     ".stack/config.yaml".source = ../stack/config.yaml;
   };
 
+  xresources.properties = {
+    # Set some Emacs GUI properties in the .Xresources file because they are
+    # expensive to set during initialization in Emacs lisp. This saves about
+    # half a second on startup time. See the following link for more options:
+    # https://www.gnu.org/software/emacs/manual/html_node/emacs/Fonts.html#Fonts
+    "Emacs.menuBar" = false;
+    "Emacs.toolBar" = false;
+    "Emacs.verticalScrollBars" = false;
+    "Emacs.Font" = "Hack:size=16";
+  };
+
   # home packages that need no extra configuration
   home.packages = with pkgs; [
     albert
