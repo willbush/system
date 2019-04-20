ade33f1087a307215de96018f9ff1640d3455a54
Will Bush <will.g.bush@gmail.com>
2019-04-20 03:02:33 -0500

    install emacs-all-the-icons-fonts via nix system config
    Dunno why I used `M-x emacs-all-the-icons-fonts` before.
    
    
    Former-commit-id: c0d15e41cb07294012a46dc00ed59d7c8a023f33

--- nixos/fonts.nix
+++ nixos/fonts.nix
@@ -5,16 +5,17 @@
     enableFontDir = true;
     enableGhostscriptFonts = true;
     fonts = with pkgs; [
-      hack-font
-      iosevka
-      source-code-pro
-      powerline-fonts
       corefonts
       dejavu_fonts
+      emacs-all-the-icons-fonts
       freefont_ttf
       google-fonts
+      hack-font
       inconsolata
+      iosevka
       liberation_ttf
+      powerline-fonts
+      source-code-pro
       terminus_font
       ttf_bitstream_vera
       ubuntu_font_family
