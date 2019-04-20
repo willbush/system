7db63893d51904449bd0518314880bcf0b4a3170
willbush <will.g.bush@gmail.com>
2019-04-19 22:46:23 -0500

    switch from package.el to home-manager for package management

--- emacs/src/init-package.el
+++ emacs/src/init-package.el
@@ -1,24 +1,32 @@
 ;;; -*- lexical-binding: t; -*-
 
-(require 'package)
+(if (eq system-type 'windows-nt)
+    (progn
+      (require 'package)
 
-(setq package-enable-at-startup nil)
+      (add-to-list 'package-archives
+                   '("melpa" . "http://melpa.org/packages/"))
+      (add-to-list 'package-archives
+                   '("melpa-stable" . "https://stable.melpa.org/packages/"))
 
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+      (setq packages-enable-at-startup nil)
+      (package-initialize)
 
-(package-initialize)
+      ;; bootstrap use-package with package.el
+      (unless (package-installed-p 'use-package)
+        (package-refresh-contents)
+        (package-install 'use-package))
 
-;; bootstrap use-package
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+      ;; ensure everything is installed
+      (setq use-package-always-ensure t))
+
+  ;; When on Linux I use use home-manager to install packages.
+  (eval-when-compile
+    (require 'package)
+    (setq packages-enable-at-startup nil)
+    (setq package-archives nil)
+    (package-initialize)))
 
 (require 'use-package)
-
-;; ensure everything is installed
-(setq use-package-always-ensure t)
 
 (provide 'init-package)
