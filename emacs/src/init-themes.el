ed5e3ddd761a020321e77223166b49bb08af766a
Will Bush <will.g.bush@gmail.com>
2019-05-11 02:26:39 -0500

    move visual related things into init-themes.el
    Former-commit-id: ffe1dc26ba56bf82ebe59af1863c944671df8003

--- emacs/src/init-themes.el
+++ emacs/src/init-themes.el
@@ -1,5 +1,34 @@
 ;;; -*- lexical-binding: t; -*-
 
+;; In Linux I set these using .Xresources file because it saves about half a
+;; second on startup time compared to setting these here.
+(when (eq system-type 'windows-nt)
+  ;; disable GUI elements.
+  (menu-bar-mode -1)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
+  ;; set default font
+  (set-face-attribute 'default nil
+                      :family "Hack"
+                      :height 110
+                      :weight 'normal
+                      :width 'normal))
+
+;; show line and column number on mode line
+(line-number-mode 1)
+(column-number-mode 1)
+
+(setq electric-pair-pairs
+  '(
+    (?\( . ?\))
+    (?\[ . ?\])
+    (?\{ . ?\})
+   ))
+(electric-pair-mode t)
+
+;; enable prettify symbols when using GUI emacs
+(when window-system (global-prettify-symbols-mode t))
+
 (use-package doom-themes
   :config
   ;; Corrects (and improves) org-mode's native fontification.
@@ -14,4 +43,10 @@
         doom-modeline-major-mode-color-icon t
         doom-modeline-buffer-file-name-style 'truncate-upto-root))
 
-(provide 'init-themes)
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package fill-column-indicator
+  :commands fci-mode)
+
+(provide 'init-visuals)
