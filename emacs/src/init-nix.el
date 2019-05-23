2ace729ad2f2da8c09410f348824d905a9d7b5e2
Will Bush <will.g.bush@gmail.com>
2019-05-22 23:47:52 -0500

    remove company-nixos-options
    It makes initial loading of nix files very slow and only works when writing top
    level expressions.

--- emacs/src/init-nix.el
+++ emacs/src/init-nix.el
@@ -1,9 +1,6 @@
 ;;; -*- lexical-binding: t; -*-
 
-(use-package nix-mode
-  :mode "\\.nix\\'"
-  :config
-  (add-to-list 'company-backends 'company-nixos-options))
+(use-package nix-mode :mode "\\.nix\\'")
 
 (use-package nix-sandbox
   :commands (nix-shell-command
@@ -11,9 +8,7 @@
              nix-compile
              nix-find-sandbox
              nix-current-sandbox
-             nix-executable-find))
-
-(use-package company-nixos-options
-  :commands company-nixos-options)
+             nix-executable-find
+             nix-find-sandbox))
 
 (provide 'init-nix)
