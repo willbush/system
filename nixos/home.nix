3ca5c81be61952e31c0275517df9fba9cf7ea643
Will Bush <will.g.bush@gmail.com>
2019-05-27 01:32:49 -0500

    emacs related nix config into its own file
    Former-commit-id: 148c49b721f8030191ac23c58589a81f7624cd1d

--- nixos/home.nix
+++ nixos/home.nix
@@ -1,12 +1,20 @@
 { config, pkgs, ... }:
 
-let home = builtins.getEnv "HOME";
+let homeDir = builtins.getEnv "HOME";
 in
 {
+  imports = [
+    ./emacs.nix
+  ];
+
   home.stateVersion = "19.03";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  home.sessionVariables =  {
+     EDITOR = "emacsclient --create-frame --alternate-editor emacs";
+  };
 
   nixpkgs.overlays =
     let path = ./overlays; in with builtins;
@@ -15,137 +23,21 @@ in
                     pathExists (path + ("/" + n + "/default.nix")))
                 (attrNames (readDir path)));
 
-  home.sessionVariables =  {
-     EDITOR = "emacsclient --create-frame --alternate-editor emacs";
-  };
-
   home.file = {
-    ".emacs.d" = {
-      source = ../emacs;
-      recursive = true;
-      # Since home-manager deploys my config and is immutable, I might as well
-      # byte compile everything deployed this way. From my profiling with `esup`
-      # I haven't been able to see much of any gain. The main advantage is an
-      # opportunity to see compiler warnings.
-      onChange = ''
-        emacs -Q --load ~/.emacs.d/init.el --batch --eval '(byte-compile-file "~/.emacs.d/init.el")'
-        emacs -Q --load ~/.emacs.d/init.el --batch --eval '(byte-recompile-directory "~/.emacs.d/src/" 0 t)'
-      '';
-    };
-
     ".config".source = ../config;
     ".config".recursive = true;
     ".xmonad/xmonad.hs".source = ../xmonad/xmonad.hs;
     # Outside of NixOS the dictionary directory needs to be set.
     # https://github.com/NixOS/nixpkgs/issues/4521
     ".aspell.conf".text = ''
-       dict-dir ${home}/.nix-profile/lib/aspell
+       dict-dir ${homeDir}/.nix-profile/lib/aspell
        master en_US
        extra-dicts en-computers.rws
        add-extra-dicts en_US-science.rws
     '';
   };
 
-  xdg = {
-    enable = true;
-    # Home manager's emacs service doesn't provide a desktop entry for the emacs
-    # client. Note the %F on the `Exec=` line passes any file name string to
-    # tell emacs to open a file. I just use Albert to launch the emacs client so
-    # I don't every really need that.
-    dataFile."applications/emacsclient.desktop".text = ''
-      [Desktop Entry]
-      Name=Emacsclient
-      GenericName=Text Editor
-      Comment=Edit text
-      MimeType=text/english;text/plain;text/x-makefile;text/x-c++hdr;text/x-c++src;text/x-chdr;text/x-csrc;text/x-java;text/x-moc;text/x-pascal;text/x-tcl;text/x-tex;application/x-shellscript;text/x-c;text/x-c++;
-      Exec=emacsclient -c -a emacs %F
-      Icon=emacs
-      Type=Application
-      Terminal=false
-      Categories=Development;TextEditor;
-      StartupWMClass=Emacs
-      Keywords=Text;Editor;
-    '';
-  };
-
-  xresources.properties = {
-    # Set some Emacs GUI properties in the .Xresources file because they are
-    # expensive to set during initialization in Emacs lisp. This saves about
-    # half a second on startup time. See the following link for more options:
-    # https://www.gnu.org/software/emacs/manual/html_node/emacs/Fonts.html#Fonts
-    "Emacs.menuBar" = false;
-    "Emacs.toolBar" = false;
-    "Emacs.verticalScrollBars" = false;
-    "Emacs.Font" = "Hack:size=16";
-  };
-
-  services.emacs.enable = true;
-  programs.emacs = {
-    enable = true;
-    # Compile with imagemagick support so I can resize images.
-    package = pkgs.emacs.override { inherit (pkgs) imagemagick; };
-    extraPackages = (epkgs: (with epkgs; [
-      attrap
-      avy
-      company
-      company-nixos-options
-      counsel
-      counsel-projectile
-      csharp-mode
-      dante
-      dashboard
-      deadgrep
-      define-word
-      dired-narrow
-      direnv
-      disk-usage
-      doom-modeline
-      doom-themes
-      esup
-      evil
-      evil-collection
-      evil-exchange
-      evil-magit
-      evil-matchit
-      evil-numbers
-      evil-surround
-      evil-tutor
-      evil-visualstar
-      expand-region
-      fd-dired
-      fill-column-indicator
-      flyspell-correct-ivy
-      general
-      git-timemachine
-      golden-ratio
-      haskell-mode
-      hindent
-      hlint-refactor
-      hydra
-      ivy
-      magit
-      markdown-mode
-      markdown-toc
-      nav-flash
-      nix-mode
-      nix-sandbox
-      nix-update
-      org-re-reveal
-      pdf-tools
-      poporg
-      powershell
-      projectile
-      rainbow-delimiters
-      ranger
-      smex
-      use-package
-      visual-fill-column
-      which-key
-      winum
-      wttrin
-      yaml-mode
-    ]));
-  };
+  xdg.enable = true;
 
   # home packages that need no extra configuration
   home.packages = with pkgs; [
@@ -207,8 +99,8 @@ in
       dropbox = "docker exec -it dropbox dropbox";
       dropbox-start = ''
       docker run -d --restart=always --name=dropbox \
-        -v ${home}/Dropbox:/dbox/Dropbox \
-        -v ${home}/.dropbox:/dbox/.dropbox \
+        -v ${homeDir}/Dropbox:/dbox/Dropbox \
+        -v ${homeDir}/.dropbox:/dbox/.dropbox \
         -e DBOX_UID=1000 -e DBOX_GID=100 janeczku/dropbox
       '';
     };
