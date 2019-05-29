d6fd21264a3cb918d0ca1eb5c4e521748635fbe6
Will Bush <will.g.bush@gmail.com>
2019-05-29 00:50:58 -0500

    add flycheck-mode to haskell mode hook

--- emacs/src/init-haskell.el
+++ emacs/src/init-haskell.el
@@ -104,6 +104,7 @@
   :commands dante-mode
   :init
   (add-hook 'haskell-mode-hook 'dante-mode)
+  (add-hook 'haskell-mode-hook 'flycheck-mode)
   (add-hook 'dante-mode-hook
             '(lambda () (flycheck-add-next-checker 'haskell-dante
                                                    '(warning . haskell-hlint)))))
