977cb923971ce447c7d9ef6115e0d85456f103d2
willbush <will.g.bush@gmail.com>
2019-05-29 03:25:07 -0500

    lower company-minimum-prefix-length back to 1
    Latest version of org mode no longer uses templates that expand with a '<'
    prefix. I was having problems with prefix length of 1 due to that. Hopefully I
    don't run into any other unforeseen problems.

--- emacs/src/init-completion.el
+++ emacs/src/init-completion.el
@@ -39,7 +39,7 @@
          ("C-p" . company-select-previous))
   :config
   (setq company-idle-delay 0.2
-        company-minimum-prefix-length 2))
+        company-minimum-prefix-length 1))
 
 (use-package counsel-projectile
   ;; When this loads, projectile will also load. It would be nice if I could use
