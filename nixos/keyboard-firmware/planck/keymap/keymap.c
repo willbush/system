90cff3559724bc282cfe7e1f1851bc8909b584da
willbush <will.g.bush@gmail.com>
2019-07-07 18:37:13 -0500

    move planck layout readme to root directory readme file

--- nixos/keyboard-firmware/planck/keymap/keymap.c
+++ nixos/keyboard-firmware/planck/keymap/keymap.c
@@ -29,6 +29,8 @@ enum planck_keycodes {
 #define HYPERSPACE MO(_HYPERSPACE)
 #define NUMPAD MO(_NUMPAD)
 
+// See the readme file at the root of this repository for easier to view tables
+// and description of these layouts.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,         KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
