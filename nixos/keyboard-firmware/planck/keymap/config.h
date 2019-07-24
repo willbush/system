f620c619049b6daa91f5b6577fb49ecd2dfc00b8
Will Bush <will.g.bush@gmail.com>
2019-07-24 04:14:06 -0500

    remove plover and super player
    I'm not too sure what dip_update function does, but the keyboard seems to work
    without it..
    
    
    Former-commit-id: 17fac8678a6d0798c8800ae3a9f935048ab5187d

--- nixos/keyboard-firmware/planck/keymap/config.h
+++ nixos/keyboard-firmware/planck/keymap/config.h
@@ -2,7 +2,6 @@
 
 #ifdef AUDIO_ENABLE
     #define STARTUP_SONG SONG(PLANCK_SOUND)
-    // #define STARTUP_SONG SONG(NO_SOUND)
 
     #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
                                   SONG(COLEMAK_SOUND), \
