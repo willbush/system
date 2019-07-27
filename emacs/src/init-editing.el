df9539a617b18ab2bea75e000a8a7ed17cb75d6b
Will Bush <will.g.bush@gmail.com>
2019-07-27 06:08:37 -0500

    fix a bunch of possible missing function warnings in init-editing.el
    Former-commit-id: 2cf3d67898154a2501f71fd9feb225336bd8d0a6

--- emacs/src/init-editing.el
+++ emacs/src/init-editing.el
@@ -1,5 +1,96 @@
 ;;; -*- lexical-binding: t; -*-
 
+;; evil-collection will give a warning if the following setting is not set
+;; before loading evil and evil-collection. Note that evil-leader loads evil
+;; see: https://github.com/emacs-evil/evil-collection/issues/215 Also even if
+;; this is in the :init block it will still given the warning when lazy loading
+;; evil.
+(setq evil-want-keybinding nil)
+
+(use-package evil
+  :hook (after-init . evil-mode)
+  :init
+  (setq evil-search-module 'evil-search
+        evil-ex-complete-emacs-commands nil
+        evil-vsplit-window-right t
+        evil-split-window-below t
+        evil-shift-round nil
+        evil-want-C-u-scroll t)
+  :config
+  ;; unbind evil-lookup
+  (eval-after-load "evil-maps"
+    (define-key evil-motion-state-map "\S-k" nil))
+
+  ;; make mnemonic alias for how I want to bind it
+  (defalias 'my/evil-search-clear-highlight 'evil-ex-nohighlight)
+  ;; evil global key bindings
+  (global-set-key (kbd "C-S-h") 'evil-window-left)
+  (global-set-key (kbd "C-S-l") 'evil-window-right)
+  (global-set-key (kbd "C-S-j") 'evil-window-down)
+  (global-set-key (kbd "C-S-k") 'evil-window-up)
+
+  (global-evil-visualstar-mode)
+  (global-evil-surround-mode 1)
+  (global-evil-matchit-mode 1)
+  (evil-exchange-cx-install)
+  (evil-collection-init))
+
+(use-package evil-collection
+  :commands evil-collection-init
+  :custom (evil-collection-setup-minibuffer t)
+  :init
+  (setq evil-collection-mode-list
+        '(calendar
+          (package-menu package)
+          (term term ansi-term multi-term)
+          compile
+          cus-theme
+          custom
+          deadgrep
+          debug
+          dired
+          disk-usage
+          ediff
+          elfeed
+          git-timemachine
+          help
+          info
+          ivy
+          man
+          minibuffer
+          (pdf pdf-view)
+          woman)))
+
+;; Enables searching via * on a visual selection.
+(use-package evil-visualstar
+  :commands global-evil-visualstar-mode)
+
+;; Enables inc/dec of numbers!
+(use-package evil-numbers
+  :commands (evil-numbers/inc-at-pt evil-numbers/dec-at-pt))
+
+(use-package evil-surround
+  :commands global-evil-surround-mode)
+
+(use-package evil-matchit
+  :commands global-evil-matchit-mode)
+
+(use-package evil-exchange
+  :commands evil-exchange-cx-install)
+
+(use-package avy :commands avy-goto-char)
+
+(use-package expand-region
+  :commands (er/expand-region er/contract-region)
+  :init
+  (defhydra hydra-expand-region ()
+     "region: "
+     ("k" er/expand-region "expand")
+     ("j" er/contract-region "contract")))
+
+(use-package evil-tutor
+  :commands evil-tutor-start)
+
 ;; For an in-depth discussion about why I'm rebinding Evil keys see the readme.
 (defun my/enable-custom-evil-keys ()
   "Sets up my custom evil key bindings for Colemak-DH keyboard layout"
@@ -129,99 +220,20 @@
     "z I" 'evil-scroll-right
     "z M" 'evil-scroll-left
     "z i" 'evil-scroll-column-right
-    "z m" 'evil-scroll-column-left))
+    "z m" 'evil-scroll-column-left)
 
-;; evil-collection will give a warning if the following setting is not set
-;; before loading evil and evil-collection. Note that evil-leader loads evil
-;; see: https://github.com/emacs-evil/evil-collection/issues/215 Also even if
-;; this is in the :init block it will still given the warning when lazy loading
-;; evil.
-(setq evil-want-keybinding nil)
+    ;; called after evil-collection makes its keybindings
+    (add-hook 'evil-collection-setup-hook
+              '(lambda (_mode mode-keymaps &rest _rest)
+                 (evil-collection-translate-key 'normal mode-keymaps
+                   "m" "h"
+                   "n" "j"
+                   "e" "k"
+                   "i" "l"
+                   "C-n" "C-j"
+                   "C-e" "C-k")))
 
-(use-package evil
-  :hook (after-init . evil-mode)
-  :init
-  (setq evil-search-module 'evil-search
-        evil-ex-complete-emacs-commands nil
-        evil-vsplit-window-right t
-        evil-split-window-below t
-        evil-shift-round nil
-        evil-want-C-u-scroll t)
-  :config
-  ;; unbind evil-lookup
-  (eval-after-load "evil-maps"
-    (define-key evil-motion-state-map "\S-k" nil))
-
-  ;; make mnemonic alias for how I want to bind it
-  (defalias 'my/evil-search-clear-highlight 'evil-ex-nohighlight)
-  ;; evil global key bindings
-  (global-set-key (kbd "C-S-h") 'evil-window-left)
-  (global-set-key (kbd "C-S-l") 'evil-window-right)
-  (global-set-key (kbd "C-S-j") 'evil-window-down)
-  (global-set-key (kbd "C-S-k") 'evil-window-up))
-
-(use-package evil-collection
-  :after evil
-  :custom (evil-collection-setup-minibuffer t)
-  :config
-  (setq evil-collection-mode-list
-        '(calendar
-          (package-menu package)
-          (term term ansi-term multi-term)
-          compile
-          cus-theme
-          custom
-          deadgrep
-          debug
-          dired
-          disk-usage
-          ediff
-          elfeed
-          git-timemachine
-          help
-          info
-          ivy
-          man
-          minibuffer
-          (pdf pdf-view)
-          woman))
-  (evil-collection-init))
-
-;; Enables searching via * on a visual selection.
-(use-package evil-visualstar
-  :after evil
-  :config (global-evil-visualstar-mode))
-
-;; Enables inc/dec of numbers!
-(use-package evil-numbers
-  :after evil
-  :commands (evil-numbers/inc-at-pt evil-numbers/dec-at-pt))
-
-(use-package evil-surround
-  :after evil
-  :config
-  (global-evil-surround-mode 1))
-
-(use-package evil-matchit
-  :after evil
-  :config (global-evil-matchit-mode 1))
-
-(use-package evil-exchange
-  :after evil
-  :config (evil-exchange-cx-install))
-
-(use-package avy :commands avy-goto-char)
-
-(use-package expand-region
-  :after hydra
-  :commands (er/expand-region er/contract-region)
-  :init
-  (defhydra hydra-expand-region ()
-     "region: "
-     ("k" er/expand-region "expand")
-     ("j" er/contract-region "contract")))
-
-(use-package evil-tutor
-  :commands evil-tutor-start)
+    ;; re-initialize all evil collection keybindings
+    (evil-collection-init))
 
 (provide 'init-editing)
