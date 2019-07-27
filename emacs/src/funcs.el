befb01494e82839f0a7ea7e0b169723e0b2f640f
Will Bush <will.g.bush@gmail.com>
2019-07-27 03:57:04 -0500

    pull in latest version of these functions from spacemacs

--- emacs/src/funcs.el
+++ emacs/src/funcs.el
@@ -3,29 +3,22 @@
 ;; text manipulation related functions:
 
 (defun my/sort-lines-by-column (&optional reverse)
-  "Sort lines by the selected column,using a visual
-block/rectangle selection. A non-nil argument sorts in REVERSE
-order."
+  "Sort lines by the selected column.
+A non-nil argument sorts in reverse order."
   (interactive "P")
-  (if (and
-       ;; is there an active selection
-       (or (region-active-p) (evil-visual-state-p))
-       ;; is it a block or rectangle selection
-       (or (eq evil-visual-selection 'block) (eq rectangle-mark-mode t))
-       ;; is the selection height 2 or more lines
-       (>= (1+ (- (line-number-at-pos (region-end))
-                  (line-number-at-pos (region-beginning)))) 2))
-      (sort-columns reverse (region-beginning) (region-end))
-    (error "Sorting by column requires a block/rect selection on 2 or more lines.")))
+  (let* ((region-active (or (region-active-p) (evil-visual-state-p)))
+         (beg (if region-active (region-beginning) (point-min)))
+         (end (if region-active (region-end) (point-max))))
+    (sort-columns reverse beg end)))
 
 (defun my/sort-lines-by-column-reverse ()
-  "Sort lines by the selected column in reverse order,using a
-visual block/rectangle selection."
+  "Sort lines by the selected column in reverse order."
   (interactive)
   (my/sort-lines-by-column -1))
 
 (defun my/sort-lines (&optional reverse)
-  "Sort lines in a region or the current buffer. A non-nil argument sorts in reverse order."
+  "Sort lines in a region or the current buffer. A non-nil
+argument sorts in reverse order."
   (interactive "P")
   (let* ((region-active (or (region-active-p) (evil-visual-state-p)))
          (beg (if region-active (region-beginning) (point-min)))
