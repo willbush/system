e17276c10669127dc69528adc286787fb01c6d29
willbush <will.g.bush@gmail.com>
2019-07-26 21:19:07 -0500

    fix package.el not yet initialized error
    Since I don't use package.el to install packages in NixOS, I shouldn't be using
    ~ensure t~

--- emacs/src/init-markdown.el
+++ emacs/src/init-markdown.el
@@ -1,7 +1,6 @@
 ;;; -*- lexical-binding: t; -*-
 
 (use-package markdown-mode
-  :ensure t
   :commands (markdown-mode gfm-mode)
   :mode (("README\\.md\\'" . gfm-mode)
          ("\\.md\\'" . markdown-mode)
