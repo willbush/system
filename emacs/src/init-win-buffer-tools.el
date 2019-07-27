cc0c96adef16d5bdf21cba8f16882268b35ea803
Will Bush <will.g.bush@gmail.com>
2019-07-27 17:10:04 -0500

    set dashboard footer icon explicitly
    Former-commit-id: 32ba5a66d3686f8f393bb8e0c0855eb16d6518f3

--- emacs/src/init-win-buffer-tools.el
+++ emacs/src/init-win-buffer-tools.el
@@ -71,16 +71,24 @@
     (add-to-list 'golden-ratio-exclude-modes ms)))
 
 (use-package dashboard
- :init
- (setq initial-buffer-choice
+  :init
+  (setq initial-buffer-choice
        (lambda ()
          (or (get-buffer "*dashboard*") (get-buffer "*scratch*"))))
- :config
- (setq dashboard-banner-logo-title nil
+  :config
+  (setq dashboard-banner-logo-title nil
        dashboard-set-heading-icons t
        dashboard-set-file-icons t
        dashboard-items '((recents  . 5)
                           (projects . 5)))
+  ;; This is the default icon, but it doesn't always show up when running Emacs
+  ;; as a daemon. So I set it explicitly here to fix the issue.
+  (setq dashboard-footer-icon
+    #("" 0 1
+      (rear-nonsticky t display
+                      (raise -0.06)
+                      font-lock-face #1=(:family "file-icons" :height 1.32 :inherit font-lock-keyword-face)
+                      face #1#)))
  (dashboard-setup-startup-hook))
 
 (use-package visual-fill-column
