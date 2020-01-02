44bfe0451aba8813dadcafb7d7b399aeeccabcbe
willbush <will.g.bush@gmail.com>
2020-01-02 01:54:03 -0600

    move alacritty into home.nix and add theme

--- nixos/home.nix
+++ nixos/home.nix
@@ -88,6 +88,39 @@ in {
     zip
   ];
 
+  programs.alacritty = {
+    enable = true;
+    # Colors (One Dark)
+    # setting examples: https://github.com/jwilm/alacritty/blob/master/alacritty.yml
+    # themes: https://github.com/eendroroy/alacritty-theme
+    settings = {
+      colors = {
+        # Using the default primary background and foreground colors because I
+        # like the dark black transparency.
+        normal = {
+          black = "0x1e2127";
+          red = "0xe06c75";
+          green = "0x98c379";
+          yellow = "0xd19a66";
+          blue = "0x61afef";
+          magenta = "0xc678dd";
+          cyan = "0x56b6c2";
+          white = "0xabb2bf";
+        };
+        bright = {
+          black = "0x5c6370";
+          red = "0xe06c75";
+          green = "0x98c379";
+          yellow = "0xd19a66";
+          blue = "0x61afef";
+          magenta = "0xc678dd";
+          cyan = "0x56b6c2";
+          white = "0xffffff";
+        };
+      };
+    };
+  };
+
   programs.git = {
     enable = true;
     userName = "willbush";
