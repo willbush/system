3b23f25077d0f8af3d0cd0658649d7837fd641a8
Will Bush <will.g.bush@gmail.com>
2020-03-21 18:46:19 -0500

    add gaming keyboard layout to planck
    Former-commit-id: 7a766edc44dc83951514bdd8fe590e645d0511e2

--- nixos/keyboard-firmware/planck/keymap/config.h
+++ nixos/keyboard-firmware/planck/keymap/config.h
@@ -3,8 +3,10 @@
 #ifdef AUDIO_ENABLE
     #define STARTUP_SONG SONG(PLANCK_SOUND)
 
+    // Using the DVORAK_SOUND below for the GAMING layer.
     #define DEFAULT_LAYER_SONGS { SONG(COLEMAK_SOUND), \
-                                  SONG(QWERTY_SOUND) \
+                                  SONG(QWERTY_SOUND),  \
+                                  SONG(DVORAK_SOUND) \
                                 }
 #endif
 
