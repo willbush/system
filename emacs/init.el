0cf4dffa96266dea5b33e2f97e7fb8f213fdc641
willbush <will.g.bush@gmail.com>
2020-04-09 05:38:55 -0500

    emacs: use early-init instead of xresources file; add gcmh

--- emacs/init.el
+++ emacs/init.el
@@ -1,22 +1,23 @@
 ;; -*- lexical-binding: t -*-
 
+;; A big contributor to startup times is garbage collection. We up the gc
+;; threshold to temporarily prevent it from running, then reset it later using a
+;; hook and controlling after that with `gcmh-mode'.
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6)
+
 ;; Keep a ref to the actual file-name-handler
-(defvar file-name-handler-alist-actual file-name-handler-alist)
+(defvar default-file-name-handler-alist file-name-handler-alist)
 
 ;; Set the file-name-handler to nil (because regexing is cpu intensive)
 (setq file-name-handler-alist nil)
 
-;; Basically disable gc during initialization
-(setq gc-cons-threshold 500000000
-      gc-cons-percentage 0.6)
-
-;; Reset gc threshold and file-name-handler-alist after initialization
-;; WARNING window-setup-hook is not called if emacs is ran with batch-mode
-(add-hook 'window-setup-hook
+;; Reset file-name-handler-alist after initialization
+(add-hook 'emacs-startup-hook
   (lambda ()
     (setq gc-cons-threshold 16777216
-          gc-cons-percentage 0.1)
-    (setq file-name-handler-alist file-name-handler-alist-actual)))
+          gc-cons-percentage 0.1
+          file-name-handler-alist default-file-name-handler-alist)))
 
 (add-to-list 'load-path (expand-file-name "src/" user-emacs-directory))
 
