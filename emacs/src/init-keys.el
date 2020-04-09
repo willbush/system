1698b562658ec23842e8438e0e9ca11679c15c16
Will Bush <will.g.bush@gmail.com>
2020-04-08 21:35:51 -0500

    emacs: remove winum
    I no longer use winum for window movement. I prefer to move around in a more
    Xmonad style.
    
    
    Former-commit-id: d2892ee8c796a5ce9d9fa8b0ad4ea88406c5172d

--- emacs/src/init-keys.el
+++ emacs/src/init-keys.el
@@ -26,15 +26,6 @@
   :keymaps 'override
   "!" 'shell-command
   "'" 'my/open-shell
-  "1" 'winum-select-window-1
-  "2" 'winum-select-window-2
-  "3" 'winum-select-window-3
-  "4" 'winum-select-window-4
-  "5" 'winum-select-window-5
-  "6" 'winum-select-window-6
-  "7" 'winum-select-window-7
-  "8" 'winum-select-window-8
-  "9" 'winum-select-window-9
   "?" 'counsel-descbinds
   "SPC" 'counsel-M-x
   "TAB" 'mode-line-other-buffer
