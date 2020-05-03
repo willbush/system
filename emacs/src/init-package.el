1dfb1209ea115e9c3a11d149769ed621c67ca5a2
willbush <will.g.bush@gmail.com>
2020-05-03 02:27:11 -0500

    emacs: update to work in Windows

--- emacs/src/init-package.el
+++ emacs/src/init-package.el
@@ -1,27 +1,31 @@
 ;;; -*- lexical-binding: t; -*-
 
+;; In Emacs 27+, package initialization occurs before `user-init-file' is
+;; loaded, but after `early-init-file'. We handle package initialization, so we
+;; must prevent Emacs from doing it early!
+(setq package-enable-at-startup nil)
+
 ;; When on Windows I use `package.el' to install packages. Otherwise, I I'm on
 ;; Linux and use an external tool called home-manager.
 (when IS-WINDOWS
-  (progn
-      (require 'package)
+  (require 'package)
 
-      (add-to-list 'package-archives
-                   '("melpa" . "https://melpa.org/packages/"))
-      (add-to-list 'package-archives
-                   '("melpa-stable" . "https://stable.melpa.org/packages/"))
-      (add-to-list 'package-archives
-                   '("gnu" . "https://elpa.gnu.org/packages/"))
+  (add-to-list 'package-archives
+               '("melpa" . "https://melpa.org/packages/"))
+  (add-to-list 'package-archives
+               '("melpa-stable" . "https://stable.melpa.org/packages/"))
+  (add-to-list 'package-archives
+               '("gnu" . "https://elpa.gnu.org/packages/"))
 
-      (package-initialize)
+  (package-initialize)
 
-      ;; bootstrap use-package with package.el
-      (unless (package-installed-p 'use-package)
-        (package-refresh-contents)
-        (package-install 'use-package))
+  ;; bootstrap use-package with package.el
+  (unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package))
 
-      ;; ensure everything is installed
-      (setq use-package-always-ensure t)))
+  ;; ensure everything is installed
+  (setq use-package-always-ensure t))
 
 ;; Despite the `use-package' README suggesting that this be wrapped in an
 ;; `eval-when-compile', that causes issues for me when I byte compile my config.
