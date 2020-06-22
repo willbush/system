51d5495b2e9d809d794903129e825cd6e720ff98
Will Bush <will.g.bush@gmail.com>
2020-06-22 14:06:38 -0500

    nixos: modify gaming layer on keyboard

--- nixos/keyboard-firmware/planck/keymap/keymap.c
+++ nixos/keyboard-firmware/planck/keymap/keymap.c
@@ -44,10 +44,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL, NUM,     KC_LGUI, KC_LALT, LOWER,   HYPER,   KC_SPC,  RAISE,   KC_RALT, KC_RGUI, _______, KC_RCTL
   ),
   [_GAMING] = LAYOUT_planck_grid(
-    KC_7,    KC_3,    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_BSPC,
-    KC_6,    KC_2,    CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_QUOT,
-    KC_5,    KC_1,    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, SFT_ENT,
-    KC_4,    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_SPC,  KC_F5,   KC_RALT, KC_RGUI, QWERTY,  COLEMAK
+    KC_6,    KC_3,    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_BSPC,
+    KC_5,    KC_2,    CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_QUOT,
+    KC_4,    KC_1,    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, SFT_ENT,
+    KC_GRV,  KC_DOT,  KC_LCTL, KC_SLSH, KC_LALT, KC_F1,   KC_SPC,  KC_F2,   KC_F3,   KC_F4,   KC_F5,   COLEMAK
   ),
   [_LOWER] = LAYOUT_planck_grid(
     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
