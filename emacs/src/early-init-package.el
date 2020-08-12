8d9be38a87b11e5210bd1345e2fd082cfa86c0a4
willbush <will.g.bush@gmail.com>
2020-08-12 13:21:43 -0500

    emacs: remove melpa-stable package archive

--- emacs/src/early-init-package.el
+++ emacs/src/early-init-package.el
@@ -14,8 +14,6 @@
   (add-to-list 'package-archives
                '("melpa" . "https://melpa.org/packages/"))
   (add-to-list 'package-archives
-               '("melpa-stable" . "https://stable.melpa.org/packages/"))
-  (add-to-list 'package-archives
                '("gnu" . "https://elpa.gnu.org/packages/"))
 
   (package-initialize)
