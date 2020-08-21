6034fe296f98c578355bfb21ada880251aba78b0
willbush <will.g.bush@gmail.com>
2020-08-20 20:13:04 -0500

    emacs: replace golden-ratio with zoom
    https://github.com/cyrus-and/zoom

--- emacs/src/init-win-buffer-tools.el
+++ emacs/src/init-win-buffer-tools.el
@@ -61,60 +61,11 @@
     "M-t" 'eyebrowse-switch-to-window-config-8
     "M-g" 'eyebrowse-switch-to-window-config-9))
 
-(use-package golden-ratio
-  :commands (golden-ratio golden-ratio-mode)
+(use-package zoom
   :config
-  ;; extra golden ratio commands
-  (dolist (cs '(avy-pop-mark
-                evil-avy-goto-word-or-subword-1
-                evil-avy-goto-line
-                evil-window-delete
-                evil-window-split
-                evil-window-vsplit
-                evil-window-left
-                evil-window-right
-                evil-window-up
-                evil-window-down
-                evil-window-bottom-right
-                evil-window-top-left
-                evil-window-mru
-                evil-window-next
-                evil-window-prev
-                evil-window-new
-                evil-window-vnew
-                evil-window-rotate-upwards
-                evil-window-rotate-downwards
-                evil-window-move-very-top
-                evil-window-move-far-left
-                evil-window-move-far-right
-                evil-window-move-very-bottom
-                next-multiframe-window
-                previous-multiframe-window
-                windmove-left
-                windmove-right
-                windmove-up
-                windmove-down
-                quit-window))
-
-    (add-to-list 'golden-ratio-extra-commands cs))
-
-  ;; modes for golden-ratio to exclude
-  (dolist (ms '("bs-mode"
-                "calc-mode"
-                "ediff-mode"
-                "eshell-mode"
-                "gud-mode"
-                "gdb-locals-mode"
-                "gdb-registers-mode"
-                "gdb-breakpoints-mode"
-                "gdb-threads-mode"
-                "gdb-frames-mode"
-                "gdb-inferior-io-mode"
-                "gdb-disassembly-mode"
-                "gdb-memory-mode"
-                "speedbar-mode"))
-
-    (add-to-list 'golden-ratio-exclude-modes ms)))
+  (custom-set-variables
+   ;; golden ratio
+  '(zoom-size '(0.618 . 0.618))))
 
 (use-package adaptive-wrap
   :commands adaptive-wrap-prefix-mode)
