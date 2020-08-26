b2ea93e85298111da3ed9699341a3e3623c79f43
Will Bush <will.g.bush@gmail.com>
2020-08-26 18:26:49 -0500

    emacs: use pandoc in markdown-mode when exporting

--- emacs/src/init-markdown.el
+++ emacs/src/init-markdown.el
@@ -5,7 +5,7 @@
   :mode (("README\\.md\\'" . gfm-mode)
          ("\\.md\\'" . markdown-mode)
          ("\\.markdown\\'" . markdown-mode))
-  :init (setq markdown-command "multimarkdown")
+  :init (setq markdown-command '("pandoc" "--from=markdown" "--to=html5"))
   (add-hook 'markdown-mode-hook
             '(lambda ()
                (setq show-trailing-whitespace t))))
