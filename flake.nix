015d9392945f5a4289b26c602c4e1f7493e000af
Will Bush <will.g.bush@gmail.com>
2020-11-01 16:30:21 -0600

    nixos: initial flakes conversion of configuration.nix
    Former-commit-id: 6ee06ce276c0bc6c84872805a461ffd14a5191d5

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "My config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # home-manager = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+  };
+
+  outputs = inputs: {
+    nixosConfigurations = {
+      betelgeuse = inputs.nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          # inputs.home-manager.nixosModules.home-manager
+          # {
+          #   home-manager.useGlobalPkgs = true;
+          #   home-manager.useUserPackages = true;
+          #   home-manager.users.will = import ./home.nix;
+          # }
+        ];
+      };
+    };
+  };
+}
