4bb403c0b15e72600ab5adf343e73d819517303e
Will Bush <will.g.bush@gmail.com>
2020-11-16 03:18:09 -0600

    nixos: add openssh to tau-ceti

--- users.nix
+++ users.nix
@@ -1,11 +1,14 @@
 { pkgs, ... }:
 
 {
-  users.extraUsers.will = {
+  users.users.will = {
     isNormalUser = true;
     home = "/home/will";
     uid = 1000;
     extraGroups = [ "wheel" "networkmanager" "docker" "libvirtd" ];
     shell = pkgs.zsh;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEE2hQsuOQZ3PvM2DdI0vxpaBFoRQpFhGXZmeRq8Srs6 tau-ceti-2020-11-16"
+    ];
   };
 }
