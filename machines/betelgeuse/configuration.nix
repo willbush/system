a7bc63b0dd2bd5039f4d090671c4fd00e2b7e2c5
willbush <will.g.bush@gmail.com>
2020-11-18 00:58:25 -0600

    emacs: change default theme

--- machines/betelgeuse/configuration.nix
+++ machines/betelgeuse/configuration.nix
@@ -1,9 +1,21 @@
 {
-  imports = [
-    ./hardware-configuration.nix
-    ../common-configuration.nix
-  ];
+  imports = [ ./hardware-configuration.nix ../common-configuration.nix ];
 
   networking.hostName = "betelgeuse";
   services.xserver.videoDrivers = [ "nvidiaBeta" ];
+
+  services.rpcbind.enable = true;
+
+  boot.supportedFilesystems = [ "nfs" ];
+
+  # fileSystems."/mnt/media" = {
+  #   device = "tau-ceti:/media";
+  #   fsType = "nfs";
+  #   options = [
+  #     "nfsvers=4.2"
+  #     "x-systemd.automount"
+  #     "noauto"
+  #     "x-systemd.idle-timeout=600" # disconnects after 10 minutes (i.e. 600 seconds)
+  #   ];
+  # };
 }
