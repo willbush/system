ae8933dd3b0dc538f61593326cb16b43a093a712
willbush <will.g.bush@gmail.com>
2020-11-20 09:25:26 -0600

    minor refactor

--- home.nix
+++ home.nix
@@ -237,31 +237,33 @@ in {
     config.theme = "TwoDark";
   };
 
-  services.picom = {
-    enable = true;
-    fade = true;
-    vSync = true;
-    experimentalBackends = true;
-    # the default 'glx' backend lags like crazy for me for some reason.
-    backend = "xrender";
-    fadeDelta = 1;
-    # I only want transparency for a couple of applications.
-    opacityRule = [
-      "95:class_g *?= 'emacs' && focused"
-      "75:class_g *?= 'emacs' && !focused"
-      "90:class_g ?= 'alacritty' && focused"
-      "75:class_g ?= 'alacritty' && !focused"
-    ];
-  };
+  services = {
+    picom = {
+      enable = true;
+      fade = true;
+      vSync = true;
+      experimentalBackends = true;
+      # the default 'glx' backend lags like crazy for me for some reason.
+      backend = "xrender";
+      fadeDelta = 1;
+      # I only want transparency for a couple of applications.
+      opacityRule = [
+        "95:class_g *?= 'emacs' && focused"
+        "75:class_g *?= 'emacs' && !focused"
+        "90:class_g ?= 'alacritty' && focused"
+        "75:class_g ?= 'alacritty' && !focused"
+      ];
+    };
 
-  services.redshift = {
-    enable = true;
-    latitude = "33";
-    longitude = "-97";
-    temperature.day = 6500;
-    temperature.night = 3000;
-  };
+    redshift = {
+      enable = true;
+      latitude = "33";
+      longitude = "-97";
+      temperature.day = 6500;
+      temperature.night = 3000;
+    };
 
-  services.lorri.enable = true;
-  services.syncthing.enable = true;
+    lorri.enable = true;
+    syncthing.enable = true;
+  };
 }
