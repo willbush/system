88b5290fde9afdf9f757b96e35cdc6aadd26dba1
Will Bush <will.g.bush@gmail.com>
2020-11-30 01:00:16 -0600

    whitespace cleaned up

--- config/zsh/zshrc-init-extra.sh
+++ config/zsh/zshrc-init-extra.sh
@@ -1,22 +1,22 @@
 # Fix tramp login hang (see: https://www.emacswiki.org/emacs/TrampMode)
 if [[ $TERM == "dumb" ]]; then
-	unsetopt zle
-	unset zle_bracketed_paste
-	export PS1='%m %~ $ '
+  unsetopt zle
+  unset zle_bracketed_paste
+  export PS1='%m %~ $ '
 fi
 
 # https://github.com/akermu/emacs-libvterm#shell-side-configuration
 vterm_printf() {
-	if [ -n "$TMUX" ]; then
-		# Tell tmux to pass the escape sequences through
-		# (Source: http://permalink.gmane.org/gmane.comp.terminal-emulators.tmux.user/1324)
-		printf "\ePtmux;\e\e]%s\007\e\\" "$1"
-	elif [ "${TERM%%-*}" = "screen" ]; then
-		# GNU screen (screen, screen-256color, screen-256color-bce)
-		printf "\eP\e]%s\007\e\\" "$1"
-	else
-		printf "\e]%s\e\\" "$1"
-	fi
+  if [ -n "$TMUX" ]; then
+    # Tell tmux to pass the escape sequences through
+    # (Source: http://permalink.gmane.org/gmane.comp.terminal-emulators.tmux.user/1324)
+    printf "\ePtmux;\e\e]%s\007\e\\" "$1"
+  elif [ "${TERM%%-*}" = "screen" ]; then
+    # GNU screen (screen, screen-256color, screen-256color-bce)
+    printf "\eP\e]%s\007\e\\" "$1"
+  else
+    printf "\e]%s\e\\" "$1"
+  fi
 }
 
 # https://github.com/akermu/emacs-libvterm#vterm-clear-scrollback
