5614d162b7d5640559e6f061795c5b0333c0b861
willbush <will.g.bush@gmail.com>
2020-12-06 19:00:57 -0600

    nixos: disable mutableUsers

--- users.nix
+++ users.nix
@@ -1,14 +1,22 @@
 { pkgs, ... }:
 
 {
-  users.users.will = {
-    isNormalUser = true;
-    home = "/home/will";
-    uid = 1000;
-    extraGroups = [ "wheel" "networkmanager" "docker" "libvirtd" ];
-    shell = pkgs.zsh;
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEE2hQsuOQZ3PvM2DdI0vxpaBFoRQpFhGXZmeRq8Srs6 tau-ceti-2020-11-16"
-    ];
+  users = {
+    mutableUsers = false;
+    users = {
+      root.hashedPassword =
+        "$6$zsnB4Yv6a$9JRJdUvKo/7GHyzjfcHwGHrDrjbD/tL0zoJbWfMtguj6cseFGTxa5DRiSCNjQWYCPoMbsTNA1ohcrmDOk4Xba0";
+      will = {
+        isNormalUser = true;
+        home = "/home/will";
+        hashedPassword =
+          "$6$cCY2ySVkvcS$NB4Q.AqH3O6CKAc8MKYjmmOu6w2cMRc5TktjkWTSKKBoXcwu9cnmfFDbMXEDEgau64jU8h1Rmf/TM6fIna2Gb1";
+        shell = pkgs.zsh;
+        extraGroups = [ "wheel" "networkmanager" "docker" "libvirtd" ];
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEE2hQsuOQZ3PvM2DdI0vxpaBFoRQpFhGXZmeRq8Srs6 tau-ceti-2020-11-16"
+        ];
+      };
+    };
   };
 }
