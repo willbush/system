b6f179ca97b82c5eae93e6a6acec8b83081ec81f
willbush <will.g.bush@gmail.com>
2020-12-31 01:02:59 -0600

    nixos: add syncthing devices and folders

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -1,6 +1,7 @@
 { config, options, lib, ... }:
 let
   inherit (lib) mkIf mkOption types elem;
+  inherit (lib.lists) remove;
   cfg = config.modules.services.syncthing;
   # Device IDs don't really need to be secret, but according to syncthing docs
   # one can get the device IP if they know the device ID.
@@ -26,22 +27,70 @@ in {
           deviceEnabled = devices: elem config.networking.hostName devices;
           deviceType = devices:
             if deviceEnabled devices then "sendreceive" else "receiveonly";
+          staggeredVersioning = {
+            type = "staggered";
+            params = {
+              cleanInterval = "3600";
+              maxAge = "31536000"; # in seconds (365 days)
+              versionsPath =
+                ".stversions"; # The default path (cannot ommit to get the default)
+            };
+          };
+
         in {
           sync = rec {
-            devices = [ "betelgeuse" "tau-ceti" ];
-            enable = deviceEnabled devices;
+            id = "mhkcv-26vrq";
             path = "/home/will/sync";
-            rescanInterval = 3600;
-            type = deviceType [ "betelgeuse" "tau-ceti" ];
+            devices =
+              [ "betelgeuse" "tau-ceti" "saiph" "alnitak" "alnilam" "rigel" ];
+            enable = deviceEnabled devices;
+            type = deviceType (remove "rigel" devices);
+            versioning = staggeredVersioning;
+          };
+          keepass = rec {
+            id = "zfp7q-qpnzd";
+            path = "/home/will/keepass";
+            devices = [
+              "betelgeuse"
+              "tau-ceti"
+              "saiph"
+              "alnitak"
+              "alnilam"
+              "bellatrix"
+              "meissa"
+              "rigel"
+            ];
+            enable = deviceEnabled devices;
+            type = deviceType (remove "rigel" devices);
+            versioning = staggeredVersioning;
+          };
+          camera = rec {
+            id = "6dmv9-w6iqp";
+            path = "/home/will/images/camera";
+            devices = [ "betelgeuse" "tau-ceti" "saiph" "alnitak" "rigel" ];
+            enable = deviceEnabled devices;
+            type = deviceType (remove "rigel" devices);
+            versioning = staggeredVersioning;
+          };
+          viofo = rec {
+            id = "1kcy7-2cg8l";
+            path = "/home/will/videos/viofo";
+            devices = [ "betelgeuse" "saiph" "alnitak" "rigel" ];
+            enable = deviceEnabled devices;
+            type = deviceType (remove "rigel" devices);
             versioning = {
-              type = "staggered";
-              params = {
-                cleanInterval = "3600";
-                maxAge = "31536000"; # in seconds (365 days)
-                versionsPath = ".stversions"; # The default path (cannot ommit to get the default)
-              };
+              type = "trashcan";
+              params.cleanoutDays = "30";
             };
-            watch = true;
+          };
+          secrets = rec {
+            id = "tuaur-mvey4";
+            label = ".secrets";
+            path = "/home/will/.secrets";
+            devices = [ "betelgeuse" "saiph" "alnitak" ];
+            enable = deviceEnabled devices;
+            type = deviceType devices;
+            versioning = staggeredVersioning;
           };
         };
       };
