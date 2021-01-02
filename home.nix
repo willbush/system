45f4b7b51fc54f910f26fbe5ba2bbf71c401aa56
Will Bush <will.g.bush@gmail.com>
2021-01-01 19:26:10 -0600

    nixos: refactor

--- home.nix
+++ home.nix
@@ -6,240 +6,230 @@ let
 in {
   imports = [ (import ./emacs.nix { inherit pkgs; }) ];
 
-  home.stateVersion = "20.09";
-  home.username = "will";
-  home.homeDirectory = "/home/will";
+  home = {
+    stateVersion = "20.09";
+    username = "will";
+    homeDirectory = "/home/will";
 
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
-  home.sessionVariables = {
-    EDITOR = "emacsclient --create-frame --alternate-editor emacs";
-    # https://github.com/sharkdp/bat#man
-    MANPAGER = "sh -c 'col -bx | ${pkgs.bat}/bin/bat -l man -p'";
-  };
-
-  home.file = {
-    ".config".source = ./config;
-    ".config".recursive = true;
-    ".xmonad/xmonad.hs".source = ./xmonad/xmonad.hs;
-    # Outside of NixOS the dictionary directory needs to be set.
-    # https://github.com/NixOS/nixpkgs/issues/4521
-    ".aspell.conf".text = ''
-      dict-dir ${homeDir}/.nix-profile/lib/aspell
-      master en_US
-      extra-dicts en-computers.rws
-    '';
-  };
-
-  xdg.enable = true;
-
-  gtk = {
-    enable = true;
-    iconTheme = {
-      name = "Adwaita";
-      package = pkgs.gnome3.adwaita-icon-theme;
+    sessionVariables = {
+      EDITOR = "emacsclient --create-frame --alternate-editor emacs";
+      # https://github.com/sharkdp/bat#man
+      MANPAGER = "sh -c 'col -bx | ${pkgs.bat}/bin/bat -l man -p'";
     };
-    theme = {
-      name = "Adwaita-dark";
-      package = pkgs.gnome3.gnome_themes_standard;
+
+    file = {
+      ".config".source = ./config;
+      ".config".recursive = true;
+      ".xmonad/xmonad.hs".source = ./xmonad/xmonad.hs;
+      # Outside of NixOS the dictionary directory needs to be set.
+      # https://github.com/NixOS/nixpkgs/issues/4521
+      ".aspell.conf".text = ''
+        dict-dir ${homeDir}/.nix-profile/lib/aspell
+        master en_US
+        extra-dicts en-computers.rws
+      '';
     };
+
+    packages = with pkgs; [
+      (firefox.override { extraNativeMessagingHosts = [ browserpass ]; })
+      albert
+      aspell
+      aspellDicts.en
+      aspellDicts.en-computers
+      bat-extras.batdiff # pretty print git diff (see: https://github.com/eth-p/bat-extras)
+      bat-extras.batgrep # pretty print ripgrep output
+      bat-extras.prettybat # formats and then bats output (useful for minified code)
+      bc
+      binutils # needed when using cabal-install outside of a nix-shell
+      cabal-install
+      cabal2nix
+      cachix
+      calibre
+      chromium
+      clang-tools
+      dnsutils
+      docker-compose
+      dotnet-sdk
+      du-dust
+      exa
+      feh
+      gcc
+      gimp
+      git-crypt
+      glances
+      gnome3.gnome-screenshot
+      gnupg
+      gopass
+      haskellPackages.cabal-plan
+      haskellPackages.ghcid
+      haskellPackages.hasktags
+      haskellPackages.hoogle
+      hicolor-icon-theme # fall back icon theme
+      keepassxc
+      libreoffice
+      mkpasswd
+      neofetch
+      niv
+      nix-prefetch-git
+      nixfmt
+      okular
+      openconnect
+      pandoc
+      pavucontrol
+      pdfgrep
+      peek
+      planck
+      python3
+      ranger
+      remmina
+      rust-analyzer
+      rustup
+      shfmt
+      shutter
+      simple-scan
+      slack
+      syncthing-cli # provides stcli
+      teams
+      texlive.combined.scheme-small # things needed for pandoc
+      tokei
+      transmission-gtk
+      unar
+      unzip
+      virt-manager
+      vlc
+      xdotool
+      xorg.xkill
+      yacreader
+      zip
+    ];
   };
 
-  home.packages = with pkgs; [
-    (firefox.override { extraNativeMessagingHosts = [ browserpass ]; })
-    albert
-    aspell
-    aspellDicts.en
-    aspellDicts.en-computers
-    bat-extras.batdiff # pretty print git diff (see: https://github.com/eth-p/bat-extras)
-    bat-extras.batgrep # pretty print ripgrep output
-    bat-extras.prettybat # formats and then bats output (useful for minified code)
-    bc
-    binutils # needed when using cabal-install outside of a nix-shell
-    cabal-install
-    cabal2nix
-    cachix
-    calibre
-    chromium
-    clang-tools
-    dnsutils
-    docker-compose
-    dotnet-sdk
-    du-dust
-    exa
-    feh
-    gcc
-    gimp
-    git-crypt
-    glances
-    gnome3.gnome-screenshot
-    gnupg
-    gopass
-    haskellPackages.cabal-plan
-    haskellPackages.ghcid
-    haskellPackages.hasktags
-    haskellPackages.hoogle
-    hicolor-icon-theme # fall back icon theme
-    keepassxc
-    libreoffice
-    mkpasswd
-    neofetch
-    niv
-    nix-prefetch-git
-    nixfmt
-    okular
-    openconnect
-    pandoc
-    pavucontrol
-    pdfgrep
-    peek
-    planck
-    python3
-    ranger
-    remmina
-    rust-analyzer
-    rustup
-    shfmt
-    shutter
-    simple-scan
-    slack
-    syncthing-cli # provides stcli
-    teams
-    texlive.combined.scheme-small # things needed for pandoc
-    tokei
-    transmission-gtk
-    unar
-    unzip
-    virt-manager
-    vlc
-    xdotool
-    xorg.xkill
-    yacreader
-    zip
-  ];
+  programs = {
+    # Let Home Manager install and manage itself.
+    home-manager.enable = true;
 
-  programs.alacritty = {
-    enable = true;
-    # Colors (One Dark)
-    # setting examples: https://github.com/jwilm/alacritty/blob/master/alacritty.yml
-    # themes: https://github.com/eendroroy/alacritty-theme
-    settings = {
-      colors = {
-        # I changed the primary background color to black because I like that
-        # with transparency.
-        primary = {
-          background = "0x000000";
-          foreground = "0xabb2bf";
-        };
-        normal = {
-          black = "0x1e2127";
-          red = "0xe06c75";
-          green = "0x98c379";
-          yellow = "0xd19a66";
-          blue = "0x61afef";
-          magenta = "0xc678dd";
-          cyan = "0x56b6c2";
-          white = "0xabb2bf";
-        };
-        bright = {
-          black = "0x5c6370";
-          red = "0xe06c75";
-          green = "0x98c379";
-          yellow = "0xd19a66";
-          blue = "0x61afef";
-          magenta = "0xc678dd";
-          cyan = "0x56b6c2";
-          white = "0xffffff";
+    alacritty = {
+      enable = true;
+      # Colors (One Dark)
+      # setting examples: https://github.com/jwilm/alacritty/blob/master/alacritty.yml
+      # themes: https://github.com/eendroroy/alacritty-theme
+      settings = {
+        colors = {
+          # I changed the primary background color to black because I like that
+          # with transparency.
+          primary = {
+            background = "0x000000";
+            foreground = "0xabb2bf";
+          };
+          normal = {
+            black = "0x1e2127";
+            red = "0xe06c75";
+            green = "0x98c379";
+            yellow = "0xd19a66";
+            blue = "0x61afef";
+            magenta = "0xc678dd";
+            cyan = "0x56b6c2";
+            white = "0xabb2bf";
+          };
+          bright = {
+            black = "0x5c6370";
+            red = "0xe06c75";
+            green = "0x98c379";
+            yellow = "0xd19a66";
+            blue = "0x61afef";
+            magenta = "0xc678dd";
+            cyan = "0x56b6c2";
+            white = "0xffffff";
+          };
         };
       };
     };
-  };
 
-  programs.git = {
-    enable = true;
-    userName = "willbush";
-    userEmail = "will.g.bush@gmail.com";
-    signing = {
-      # public key fingerprint
-      key = "4441422E61E4C8F3EBFE5E333823864B54B13BDA";
-      signByDefault = true;
-    };
-  };
-
-  programs.fzf = {
-    enable = true;
-    # This defaults to true, but I want to make it explicit because installing
-    # fzf this way is different than putting it in the home.packages list.
-    enableZshIntegration = true;
-  };
-
-  programs.zsh = {
-    enable = true;
-    dotDir = ".config/zsh";
-    enableCompletion = true;
-    enableAutosuggestions = true;
-    shellAliases = {
-      l = "exa";
-      ll = "exa -l";
-      la = "exa -lah";
-      vim = "nvim";
-    };
-
-    oh-my-zsh = {
+    git = {
       enable = true;
-      plugins = [
-        # Completion providers:
-        "ripgrep"
-        "cargo"
-        "rust"
-        "fd"
-      ];
+      userName = "willbush";
+      userEmail = "will.g.bush@gmail.com";
+      signing = {
+        # public key fingerprint
+        key = "4441422E61E4C8F3EBFE5E333823864B54B13BDA";
+        signByDefault = true;
+      };
     };
 
-    history = {
-      path = "${config.xdg.dataHome}/zsh/zsh_history";
-      extended = false; # Whether to insert timestamps
-      ignoreDups = true;
-      size = 100000;
-      save = 100000;
+    fzf = {
+      enable = true;
+      # This defaults to true, but I want to make it explicit because installing
+      # fzf this way is different than putting it in the home.packages list.
+      enableZshIntegration = true;
     };
-    initExtra = pkgs.lib.fileContents ./config/zsh/zshrc-init-extra.sh;
-  };
 
-  programs.starship.enable = true;
+    zsh = {
+      enable = true;
+      dotDir = ".config/zsh";
+      enableCompletion = true;
+      enableAutosuggestions = true;
+      shellAliases = {
+        l = "exa";
+        ll = "exa -l";
+        la = "exa -lah";
+        vim = "nvim";
+      };
 
-  programs.broot = {
-    enable = true;
-    enableZshIntegration = true;
-  };
+      oh-my-zsh = {
+        enable = true;
+        plugins = [
+          # Completion providers:
+          "ripgrep"
+          "cargo"
+          "rust"
+          "fd"
+        ];
+      };
 
-  programs.htop = {
-    enable = true;
-    meters = {
-      left = [ "AllCPUs" "Memory" "Swap" ];
-      right = [ "Tasks" "LoadAverage" "Uptime" ];
+      history = {
+        path = "${config.xdg.dataHome}/zsh/zsh_history";
+        extended = false; # Whether to insert timestamps
+        ignoreDups = true;
+        size = 100000;
+        save = 100000;
+      };
+      initExtra = pkgs.lib.fileContents ./config/zsh/zshrc-init-extra.sh;
     };
-  };
 
-  programs.direnv = {
-    enable = true;
-    enableZshIntegration = true;
-  };
+    starship.enable = true;
 
-  programs.ssh = {
-    enable = true;
-    serverAliveInterval = 30;
-
-    matchBlocks."github" = {
-      hostname = "github.com";
-      identityFile = "~/.secrets/id_rsa_github";
+    broot = {
+      enable = true;
+      enableZshIntegration = true;
     };
-  };
 
-  programs.bat = {
-    enable = true;
-    config.theme = "TwoDark";
+    htop = {
+      enable = true;
+      meters = {
+        left = [ "AllCPUs" "Memory" "Swap" ];
+        right = [ "Tasks" "LoadAverage" "Uptime" ];
+      };
+    };
+
+    direnv = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    ssh = {
+      enable = true;
+      serverAliveInterval = 30;
+
+      matchBlocks."github" = {
+        hostname = "github.com";
+        identityFile = "~/.secrets/id_rsa_github";
+      };
+    };
+
+    bat = {
+      enable = true;
+      config.theme = "TwoDark";
+    };
   };
 
   services = {
@@ -269,5 +259,19 @@ in {
     };
 
     lorri.enable = true;
+  };
+
+  xdg.enable = true;
+
+  gtk = {
+    enable = true;
+    iconTheme = {
+      name = "Adwaita";
+      package = pkgs.gnome3.adwaita-icon-theme;
+    };
+    theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome3.gnome_themes_standard;
+    };
   };
 }
