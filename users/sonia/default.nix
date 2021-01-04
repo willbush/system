6923fe34baf16814d0bbdf3dcb88fe2868660c4e
Will Bush <will.g.bush@gmail.com>
2021-01-03 23:31:04 -0600

    nixos: extract common nix settings
    Former-commit-id: 87e67e3f6e41e69066a195a72254dba9e686eeb4

--- users/sonia/default.nix
+++ users/sonia/default.nix
@@ -1,7 +1,8 @@
 { config, lib, pkgs, ... }:
 let inherit (lib) fileContents;
 in {
-  imports = [ ../../profiles/common/fonts.nix ];
+  imports =
+    [ ../../profiles/common/fonts.nix ../../profiles/common/nix-settings.nix ];
   # TODO deal with syncthing hard-coding home paths
   # imports = [ ../../fonts.nix ../../modules/services/syncthing.nix ];
 
@@ -28,38 +29,6 @@ in {
   };
 
   nixpkgs.config.allowUnfree = true;
-
-  nix = {
-    # Required until nix version 2.4 for nix flakes
-    package = pkgs.nixUnstable;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-
-    autoOptimiseStore = true;
-    binaryCaches = [
-      "https://cache.nixos.org/"
-      "https://hercules-ci.cachix.org"
-      "https://iohk.cachix.org"
-      "https://nix-community.cachix.org"
-      "https://nix-tools.cachix.org"
-      "https://nixcache.reflex-frp.org"
-      "https://willbush.cachix.org"
-    ];
-    binaryCachePublicKeys = [
-      "hercules-ci.cachix.org-1:ZZeDl9Va+xe9j+KqdzoBZMFJHVQ42Uu/c/1/KMC5Lw0="
-      "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "nix-tools.cachix.org-1:ebBEBZLogLxcCvipq2MTvuHlP7ZRdkazFSQsbs0Px1A="
-      "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
-      "willbush.cachix.org-1:PuQjKarzPYTnxgEzKUoTDQ+aN0SImhO8NMZ0CamKBL4="
-    ];
-    gc = {
-      automatic = true;
-      dates = "23:00";
-      options = "--delete-older-than 30d";
-    };
-  };
 
   networking = {
     firewall.enable = true;
@@ -112,14 +81,7 @@ in {
   };
 
   # List packages installed in system profile.
-  environment.systemPackages = with pkgs; [
-    curl
-    fd
-    neovim
-    ripgrep
-    tree
-    wget
-  ];
+  environment.systemPackages = with pkgs; [ curl fd neovim ripgrep tree wget ];
 
   programs = {
     ssh.startAgent = true;
--- users/will/default.nix
+++ users/will/default.nix
@@ -1,7 +1,11 @@
 { config, lib, pkgs, ... }:
 let inherit (lib) fileContents;
 in {
-  imports = [ ../../profiles/common/fonts.nix ../../modules/services/syncthing.nix ];
+  imports = [
+    ../../modules/services/syncthing.nix
+    ../../profiles/common/fonts.nix
+    ../../profiles/common/nix-settings.nix
+  ];
 
   users = {
     mutableUsers = false;
@@ -32,37 +36,6 @@ in {
   nixpkgs.config = {
     # Allow unfree, which is required for some drivers.
     allowUnfree = true;
-  };
-
-  nix = {
-    # Required until nix version 2.4 for nix flakes
-    package = pkgs.nixUnstable;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-    autoOptimiseStore = true;
-    binaryCaches = [
-      "https://cache.nixos.org/"
-      "https://hercules-ci.cachix.org"
-      "https://iohk.cachix.org"
-      "https://nix-community.cachix.org"
-      "https://nix-tools.cachix.org"
-      "https://nixcache.reflex-frp.org"
-      "https://willbush.cachix.org"
-    ];
-    binaryCachePublicKeys = [
-      "hercules-ci.cachix.org-1:ZZeDl9Va+xe9j+KqdzoBZMFJHVQ42Uu/c/1/KMC5Lw0="
-      "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "nix-tools.cachix.org-1:ebBEBZLogLxcCvipq2MTvuHlP7ZRdkazFSQsbs0Px1A="
-      "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
-      "willbush.cachix.org-1:PuQjKarzPYTnxgEzKUoTDQ+aN0SImhO8NMZ0CamKBL4="
-    ];
-    gc = {
-      automatic = true;
-      dates = "23:00";
-      options = "--delete-older-than 30d";
-    };
   };
 
   networking = {
