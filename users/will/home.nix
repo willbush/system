40638d1aca53f94da19731e38182b011e4d27edc
Will Bush <will.g.bush@gmail.com>
2021-01-09 02:36:01 -0600

    nixos: refactor extract alacritty

--- users/will/home.nix
+++ users/will/home.nix
@@ -2,8 +2,12 @@
 
 let planck = pkgs.callPackage ../../keyboard-firmware/planck { };
 in {
-  imports =
-    [ ../profiles/bat.nix ../profiles/emacs.nix ../profiles/packages.nix ];
+  imports = [
+    ../profiles/alacritty.nix
+    ../profiles/bat.nix
+    ../profiles/emacs.nix
+    ../profiles/packages.nix
+  ];
 
   home = rec {
     stateVersion = "20.09";
@@ -60,43 +64,6 @@ in {
   };
 
   programs = {
-    alacritty = {
-      enable = true;
-      # Colors (One Dark)
-      # setting examples: https://github.com/jwilm/alacritty/blob/master/alacritty.yml
-      # themes: https://github.com/eendroroy/alacritty-theme
-      settings = {
-        colors = {
-          # I changed the primary background color to black because I like that
-          # with transparency.
-          primary = {
-            background = "0x000000";
-            foreground = "0xabb2bf";
-          };
-          normal = {
-            black = "0x1e2127";
-            red = "0xe06c75";
-            green = "0x98c379";
-            yellow = "0xd19a66";
-            blue = "0x61afef";
-            magenta = "0xc678dd";
-            cyan = "0x56b6c2";
-            white = "0xabb2bf";
-          };
-          bright = {
-            black = "0x5c6370";
-            red = "0xe06c75";
-            green = "0x98c379";
-            yellow = "0xd19a66";
-            blue = "0x61afef";
-            magenta = "0xc678dd";
-            cyan = "0x56b6c2";
-            white = "0xffffff";
-          };
-        };
-      };
-    };
-
     git = {
       enable = true;
       userName = "willbush";
