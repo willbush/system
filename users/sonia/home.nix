033ff4ec67787d8466a5b5ecc53a4e4633a01ba0
Will Bush <will.g.bush@gmail.com>
2021-01-09 02:36:01 -0600

    nixos: refactor extract alacritty
    Former-commit-id: 40638d1aca53f94da19731e38182b011e4d27edc

--- users/sonia/home.nix
+++ users/sonia/home.nix
@@ -4,6 +4,7 @@
       inherit pkgs;
       emacsPackage = pkgs.emacsGit;
     })
+    ../profiles/alacritty.nix
     ../profiles/bat.nix
     ../profiles/packages.nix
   ];
@@ -38,43 +39,6 @@
   };
 
   programs = {
-    alacritty = {
-      enable = true;
-      # Colors (One Dark)
-      # setting examples: https://github.com/jwilm/alacritty/blob/master/alacritty.yml
-      # themes: https://github.com/eendroroy/alacritty-theme
-      settings = {
-        colors = {
-          # I changed the primary background color to black because I like that
-          # with transparency.
-          primary = {
-            background = "0x000000";
-            foreground = "0xabb2bf";
-          };
-          normal = {
-            black = "0x1e2127";
-            red = "0xe06c75";
-            green = "0x98c379";
-            yellow = "0xd19a66";
-            blue = "0x61afef";
-            magenta = "0xc678dd";
-            cyan = "0x56b6c2";
-            white = "0xabb2bf";
-          };
-          bright = {
-            black = "0x5c6370";
-            red = "0xe06c75";
-            green = "0x98c379";
-            yellow = "0xd19a66";
-            blue = "0x61afef";
-            magenta = "0xc678dd";
-            cyan = "0x56b6c2";
-            white = "0xffffff";
-          };
-        };
-      };
-    };
-
     # TODO
     git = {
       enable = true;
