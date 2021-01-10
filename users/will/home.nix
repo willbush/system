1ee38096c591fe5af9b979438a799bb195a601c6
willbush <will.g.bush@gmail.com>
2021-01-09 21:23:23 -0600

    nixos: move home services only I'll use back to my home.nix

--- users/will/home.nix
+++ users/will/home.nix
@@ -7,7 +7,6 @@ in {
     ../profiles/emacs.nix
     ../profiles/packages.nix
     ../profiles/programs.nix
-    ../profiles/services.nix
   ];
 
   home = rec {
@@ -68,7 +67,33 @@ in {
     };
   };
 
-  services.lorri.enable = true;
+  services = {
+    picom = {
+      enable = true;
+      fade = true;
+      vSync = true;
+      experimentalBackends = true;
+      # the default 'glx' backend lags like crazy for me for some reason.
+      backend = "xrender";
+      fadeDelta = 1;
+      # I only want transparency for a couple of applications.
+      opacityRule = [
+        "95:class_g *?= 'emacs' && focused"
+        "75:class_g *?= 'emacs' && !focused"
+        "90:class_g ?= 'alacritty' && focused"
+        "75:class_g ?= 'alacritty' && !focused"
+      ];
+    };
+
+    redshift = {
+      enable = true;
+      latitude = "33";
+      longitude = "-97";
+      temperature.day = 6500;
+      temperature.night = 3000;
+    };
+    lorri.enable = true;
+  };
 
   xdg.enable = true;
 
