0aa6b49c8aed2defe2c5224b6477eaeb65ccb15a
willbush <will.g.bush@gmail.com>
2021-01-18 00:50:15 -0600

    win: update windows related install steps

--- config-backups/win/mklinks.bat
+++ config-backups/win/mklinks.bat
@@ -1,6 +1,5 @@
 set scriptPath=%~dp0
 
-mklink /J %USERPROFILE%\.emacs.d %scriptPath%..\emacs
 mklink %USERPROFILE%\.gitconfig %scriptPath%\config\.gitconfig
 mklink %USERPROFILE%\.vsvimrc %scriptPath%\config\.vsvimrc
 
