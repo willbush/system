90f3d22cd0fc9312838124fb344463994cb593c4
willbush <will.g.bush@gmail.com>
2021-01-24 21:01:14 -0600

    nixos: move pkgs only used by emacs to emacs.nix

--- users/profiles/emacs.nix
+++ users/profiles/emacs.nix
@@ -1,11 +1,24 @@
 { pkgs, emacsPackage ? pkgs.emacsGcc, ... }: {
 
+  home.file = {
+    ".emacs.d" = {
+      source = ../../emacs;
+      recursive = true;
+    };
+  };
+
   services.emacs = {
     enable = true;
     # temporarily disable to work around a desktop file collision issue
     # https://github.com/nix-community/emacs-overlay/issues/58
     # client.enable = true;
   };
+
+  home.packages = with pkgs; [
+    aspell
+    aspellDicts.en
+    aspellDicts.en-computers
+  ];
 
   programs.emacs = {
     enable = true;
@@ -94,12 +107,5 @@
         yasnippet
         zoom
       ]));
-  };
-
-  home.file = {
-    ".emacs.d" = {
-      source = ../../emacs;
-      recursive = true;
-    };
   };
 }
