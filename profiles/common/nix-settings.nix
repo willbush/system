70d8b97cfdad65ba80a73fadd9a7c767658b63c8
willbush <will.g.bush@gmail.com>
2021-04-24 00:32:07 -0500

    nixos: add iohk binary cache

--- profiles/common/nix-settings.nix
+++ profiles/common/nix-settings.nix
@@ -10,7 +10,7 @@
     binaryCaches = [
       "https://cache.nixos.org/"
       "https://hercules-ci.cachix.org"
-      "https://iohk.cachix.org"
+      "https://hydra.iohk.io"
       "https://nix-community.cachix.org"
       "https://nix-tools.cachix.org"
       "https://nixcache.reflex-frp.org"
@@ -18,7 +18,7 @@
     ];
     binaryCachePublicKeys = [
       "hercules-ci.cachix.org-1:ZZeDl9Va+xe9j+KqdzoBZMFJHVQ42Uu/c/1/KMC5Lw0="
-      "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nix-tools.cachix.org-1:ebBEBZLogLxcCvipq2MTvuHlP7ZRdkazFSQsbs0Px1A="
       "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
