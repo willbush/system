b1001a4509734db39b7283918f5d5d5af925cf24
Will Bush <will.g.bush@gmail.com>
2021-05-31 18:49:27 -0500

    emacs: add some default erc settings

---
+++ emacs/src/init-erc.el
@@ -0,0 +1,18 @@
+;;; -*- lexical-binding: t; -*-
+
+(use-package erc
+  :init
+  (setq erc-server "irc.libera.chat"
+        erc-nick "willbush"
+        erc-port 6697 ;; TLS port number
+
+        erc-autojoin-channels-alist
+        '(("libera.chat"
+           "##rust"
+           "#emacs"
+           "#haskell"
+           "#haskell-beginners"
+           "#nixos"
+           "#nixos-emacs"))))
+
+(provide 'init-erc)
