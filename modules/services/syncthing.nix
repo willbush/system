b7ceb299dcfa5934ba6b894327ad32da391ecb42
Will Bush <will.g.bush@gmail.com>
2021-08-09 00:57:40 -0500

    nixos: update and remove no longer `declarative` from syncthing module
    Former-commit-id: 3b60ba6d98662e4b012a4b73f2a576719a1c7dff

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -28,112 +28,110 @@ in {
       configDir = "/home/${cfg.user}/.config/syncthing";
       dataDir = "/home/${cfg.user}/.local/share/syncthing";
 
-      declarative = {
-        inherit devices;
+      inherit devices;
 
-        #  A device ID is generated from the SHA-256 of certificate information.
-        #  Therefore, use our own cert and key to prevent device ID's from
-        #  changing on a fresh install of NixOS. see
-        #  https://docs.syncthing.net/dev/device-ids.html
-        #
-        # Note I'm writing the text of the pem files into the nix store because
-        # otherwise I randomly get a "syncthing-copy-keys cannot stat" error. I
-        # guess due to the timing of when the systemd service starts and it runs
-        # that syncthing-copy-keys script and/or the fact those files are in a
-        # git-crypt folder.
-        cert = "${pkgs.writeText "syncthing-cert.pem" cert-text}";
-        key = "${pkgs.writeText "syncthing-key.pem" key-text}";
+      #  A device ID is generated from the SHA-256 of certificate information.
+      #  Therefore, use our own cert and key to prevent device ID's from
+      #  changing on a fresh install of NixOS. see
+      #  https://docs.syncthing.net/dev/device-ids.html
+      #
+      # Note I'm writing the text of the pem files into the nix store because
+      # otherwise I randomly get a "syncthing-copy-keys cannot stat" error. I
+      # guess due to the timing of when the systemd service starts and it runs
+      # that syncthing-copy-keys script and/or the fact those files are in a
+      # git-crypt folder.
+      cert = "${pkgs.writeText "syncthing-cert.pem" cert-text}";
+      key = "${pkgs.writeText "syncthing-key.pem" key-text}";
 
-        folders = let
-          deviceEnabled = devices: elem host devices;
-          deviceType = devices:
-            if deviceEnabled devices then "sendreceive" else "receiveonly";
-          staggeredVersioning = {
-            type = "staggered";
-            params = {
-              cleanInterval = "3600";
-              maxAge = "31536000"; # in seconds (365 days)
-              versionsPath =
-                ".stversions"; # The default path (cannot ommit to get the default)
-            };
+      folders = let
+        deviceEnabled = devices: elem host devices;
+        deviceType = devices:
+          if deviceEnabled devices then "sendreceive" else "receiveonly";
+        staggeredVersioning = {
+          type = "staggered";
+          params = {
+            cleanInterval = "3600";
+            maxAge = "31536000"; # in seconds (365 days)
+            versionsPath =
+              ".stversions"; # The default path (cannot ommit to get the default)
           };
+        };
 
-        in {
-          sync-will = rec {
-            label = "sync";
-            id = "mhkcv-26vrq";
-            path = "/home/${cfg.user}/sync";
-            devices =
-              [ "betelgeuse" "tau-ceti" "saiph" "alnitak" "alnilam" "rigel" ];
-            enable = deviceEnabled devices;
-            type = deviceType (remove "rigel" devices);
-            versioning = staggeredVersioning;
+      in {
+        sync-will = rec {
+          label = "sync";
+          id = "mhkcv-26vrq";
+          path = "/home/${cfg.user}/sync";
+          devices =
+            [ "betelgeuse" "tau-ceti" "saiph" "alnitak" "alnilam" "rigel" ];
+          enable = deviceEnabled devices;
+          type = deviceType (remove "rigel" devices);
+          versioning = staggeredVersioning;
+        };
+        sync-sonia = rec {
+          label = "sync";
+          id = "my5ji-1zcu3";
+          path = "/home/${cfg.user}/sync";
+          devices = [ "bellatrix" "meissa" "rigel" ];
+          enable = deviceEnabled devices;
+          type = deviceType (remove "rigel" devices);
+          versioning = staggeredVersioning;
+        };
+        camera-will = rec {
+          label = "camera";
+          id = "6dmv9-w6iqp";
+          path = "/home/${cfg.user}/images/camera";
+          devices = [ "betelgeuse" "tau-ceti" "saiph" "alnitak" "rigel" ];
+          enable = deviceEnabled devices;
+          type = deviceType (remove "rigel" devices);
+          versioning = staggeredVersioning;
+        };
+        camera-sonia = rec {
+          label = "camera";
+          id = "qtew9-yp0z9";
+          path = "/home/${cfg.user}/images/camera";
+          devices = [ "bellatrix" "meissa" "rigel" ];
+          enable = deviceEnabled devices;
+          type = deviceType (remove "rigel" devices);
+          versioning = staggeredVersioning;
+        };
+        keepass = rec {
+          id = "zfp7q-qpnzd";
+          path = "/home/${cfg.user}/keepass";
+          devices = [
+            "betelgeuse"
+            "tau-ceti"
+            "saiph"
+            "alnitak"
+            "alnilam"
+            "bellatrix"
+            "meissa"
+            "rigel"
+          ];
+          enable = deviceEnabled devices;
+          type = deviceType (remove "rigel" devices);
+          versioning = staggeredVersioning;
+        };
+        viofo = rec {
+          id = "1kcy7-2cg8l";
+          path = "/home/${cfg.user}/videos/viofo";
+          devices = [ "betelgeuse" "saiph" "alnitak" "rigel" ];
+          enable = deviceEnabled devices;
+          type = deviceType (remove "rigel" devices);
+          versioning = {
+            type = "trashcan";
+            params.cleanoutDays = "30";
           };
-          sync-sonia = rec {
-            label = "sync";
-            id = "my5ji-1zcu3";
-            path = "/home/${cfg.user}/sync";
-            devices = [ "bellatrix" "meissa" "rigel" ];
-            enable = deviceEnabled devices;
-            type = deviceType (remove "rigel" devices);
-            versioning = staggeredVersioning;
-          };
-          camera-will = rec {
-            label = "camera";
-            id = "6dmv9-w6iqp";
-            path = "/home/${cfg.user}/images/camera";
-            devices = [ "betelgeuse" "tau-ceti" "saiph" "alnitak" "rigel" ];
-            enable = deviceEnabled devices;
-            type = deviceType (remove "rigel" devices);
-            versioning = staggeredVersioning;
-          };
-          camera-sonia = rec {
-            label = "camera";
-            id = "qtew9-yp0z9";
-            path = "/home/${cfg.user}/images/camera";
-            devices = [ "bellatrix" "meissa" "rigel" ];
-            enable = deviceEnabled devices;
-            type = deviceType (remove "rigel" devices);
-            versioning = staggeredVersioning;
-          };
-          keepass = rec {
-            id = "zfp7q-qpnzd";
-            path = "/home/${cfg.user}/keepass";
-            devices = [
-              "betelgeuse"
-              "tau-ceti"
-              "saiph"
-              "alnitak"
-              "alnilam"
-              "bellatrix"
-              "meissa"
-              "rigel"
-            ];
-            enable = deviceEnabled devices;
-            type = deviceType (remove "rigel" devices);
-            versioning = staggeredVersioning;
-          };
-          viofo = rec {
-            id = "1kcy7-2cg8l";
-            path = "/home/${cfg.user}/videos/viofo";
-            devices = [ "betelgeuse" "saiph" "alnitak" "rigel" ];
-            enable = deviceEnabled devices;
-            type = deviceType (remove "rigel" devices);
-            versioning = {
-              type = "trashcan";
-              params.cleanoutDays = "30";
-            };
-          };
-          secrets = rec {
-            id = "tuaur-mvey4";
-            label = ".secrets";
-            path = "/home/${cfg.user}/.secrets";
-            devices =
-              [ "betelgeuse" "tau-ceti" "saiph" "alnitak" "alnilam" "rigel" ];
-            enable = deviceEnabled devices;
-            type = deviceType devices;
-            versioning = staggeredVersioning;
-          };
+        };
+        secrets = rec {
+          id = "tuaur-mvey4";
+          label = ".secrets";
+          path = "/home/${cfg.user}/.secrets";
+          devices =
+            [ "betelgeuse" "tau-ceti" "saiph" "alnitak" "alnilam" "rigel" ];
+          enable = deviceEnabled devices;
+          type = deviceType devices;
+          versioning = staggeredVersioning;
         };
       };
     };
