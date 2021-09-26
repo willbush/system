70234448a3c2aec7a4607b9a4e2775f232b7f4a3
Will Bush <will.g.bush@gmail.com>
2021-09-26 16:42:18 -0500

    nixos: remove wsl.nix and home-manager activation for wsl on non-nixos systems
    This was for running home-manager in WSL on a non-nixos distro. I decided to
    just use nixos-wsl (use NIXOS distro for WSL).
    
    
    Former-commit-id: 5bea3661381a2cfbe121cebef2958721db5268b4

--- flake.nix
+++ flake.nix
@@ -38,34 +38,12 @@
             { nixpkgs.overlays = [ inputs.emacs-overlay.overlay ]; }
           ];
         };
-    in {
+    in
+    {
       packages.${system}.iso =
         self.nixosConfigurations.iso.config.system.build.isoImage;
 
       nixosConfigurations = genAttrs hosts toConfig;
-
-      homeConfigurations = {
-        wsl = inputs.home-manager.lib.homeManagerConfiguration {
-          system = "x86_64-linux";
-          homeDirectory = "/home/will";
-          username = "will";
-
-          # configuration = ./home/hosts/wsl.nix;
-          configuration = {
-            imports = [ ./hosts/wsl.nix ];
-
-            nixpkgs.overlays = [ inputs.emacs-overlay.overlay ];
-            # xdg.configFile."nix/nix.conf".text = ''
-            #     experimental-features = nix-command flakes
-            #     substituters = https://cache.nixos.org https://nix-community.cachix.org
-            #     trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
-            # '';
-          };
-        };
-      };
-
-      # activate home-manager configuration with `nix run .#wsl`
-      wsl = self.homeConfigurations.wsl.activationPackage;
 
       # This is a work around for `nix repl` not yet supporting flakes.
       # You can enter a repl for this flake be doing: nix run '.#repl'
