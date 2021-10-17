23df5f470e9fb13459579405f81cd1c7a5eb72fd
Will Bush <will.g.bush@gmail.com>
2021-10-17 00:12:48 -0500

    nixos: extract gtk settings
    Former-commit-id: 6721a0e97ebb9918695d7d99a3f6ab68628e8f62

---
+++ users/profiles/gtk.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }:
+{
+  gtk = {
+    enable = true;
+    iconTheme = {
+      name = "Adwaita";
+      package = pkgs.gnome3.adwaita-icon-theme;
+    };
+    theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome3.gnome_themes_standard;
+    };
+  };
+}
