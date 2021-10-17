169b0835440d9615f3a50e2f5ab58f71798e8214
Will Bush <will.g.bush@gmail.com>
2021-10-17 00:13:09 -0500

    nixos: move some programs

--- users/will/home.nix
+++ users/will/home.nix
@@ -83,22 +83,5 @@ in
     ];
   };
 
-  programs = {
-    neovim = {
-      enable = true;
-      extraConfig = builtins.readFile ../../nvim/init.vim;
-    };
-
-    broot = {
-      enable = true;
-      enableZshIntegration = true;
-    };
-
-    direnv = {
-      enable = true;
-      enableZshIntegration = true;
-    };
-  };
-
   services.lorri.enable = true;
 }
