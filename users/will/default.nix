32d57794bcdc0a74bd021520d9ce0dc37f5d98a9
Will Bush <will.g.bush@gmail.com>
2021-10-17 02:12:00 -0500

    nixos: inline home.nix for will user
    Former-commit-id: f178e540cdf30aa73b1afea0ebf10b7b8ed3c50c

--- users/will/default.nix
+++ users/will/default.nix
@@ -26,7 +26,44 @@ in
     };
   };
 
-  home-manager.users.will = import ./home.nix;
+  home-manager.users.will = {
+    imports = [
+      (import ../profiles/emacs.nix {
+        inherit pkgs;
+        emacsPackage = pkgs.emacsGcc;
+      })
+      ../profiles/bat.nix
+      ../profiles/gpg.nix
+      ../profiles/pkgs/cli.nix
+      ../profiles/pkgs/gui.nix
+      ../profiles/programs.nix
+      ../profiles/redshift.nix
+      ./gtk.nix
+      ./picom.nix
+      ./pkgs/cli.nix
+      ./pkgs/gui.nix
+      ./rofi.nix
+      ./xdg.nix
+    ];
+
+    home = rec {
+      stateVersion = "21.11";
+      username = "will";
+      homeDirectory = "/home/will";
+
+      sessionVariables = {
+        EDITOR = "emacsclient --create-frame --alternate-editor emacs";
+      };
+
+      file = {
+        ".config".source = ../../config;
+        ".config".recursive = true;
+        ".xmonad/xmonad.hs".source = ../../xmonad/xmonad.hs;
+      };
+    };
+
+    services.lorri.enable = true;
+  };
 
   modules.unfree.allowList = [ "discord" "slack" "teams" ];
 
