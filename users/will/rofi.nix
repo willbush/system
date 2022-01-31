8f4230e36bdca3a88ef3b1ab470f76d93ef62715
Will Bush <will.g.bush@gmail.com>
2022-01-30 21:57:56 -0600

    nixos: add rofi-calc

--- users/will/rofi.nix
+++ users/will/rofi.nix
@@ -5,6 +5,9 @@
     theme = "Arc-Dark";
     pass.enable = true;
     font = "Hack Nerd Font Mono 12";
+    plugins = with pkgs; [
+      rofi-calc
+    ];
   };
 
   home.packages = with pkgs; [ rofi-power-menu ];
