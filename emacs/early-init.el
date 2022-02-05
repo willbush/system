23355af81cfc48e063e0657309fe269bdd22be90
Will Bush <will.g.bush@gmail.com>
2022-02-04 21:36:29 -0600

    emacs: change default theme

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -47,7 +47,7 @@
 (use-package doom-themes
   :config
   ;; Apply `doom-theme'
-  (load-theme 'doom-moonlight t))
+  (load-theme 'doom-one t))
 
 ;; Set `doom-modeline' early to prevent non-stylized UI flash.
 ;; Note: `doom-modeline' requires M-x all-the-icons-install-fonts.
