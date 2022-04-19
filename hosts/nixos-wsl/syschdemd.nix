18abbcd0437de618135fef21a7e3da469785ab77
Will Bush <will.g.bush@gmail.com>
2022-04-19 15:07:17 -0500

    nixos: update nixos-wsl from upstream

--- hosts/nixos-wsl/syschdemd.nix
+++ hosts/nixos-wsl/syschdemd.nix
@@ -1,4 +1,10 @@
-{ lib, pkgs, config, defaultUser, ... }:
+{ lib
+, pkgs
+, config
+, defaultUser
+, defaultUserHome ? "/home/${defaultUser}"
+, ...
+}:
 
 pkgs.substituteAll {
   name = "syschdemd";
@@ -8,8 +14,13 @@ pkgs.substituteAll {
 
   buildInputs = with pkgs; [ daemonize ];
 
+  inherit defaultUser defaultUserHome;
   inherit (pkgs) daemonize;
-  inherit defaultUser;
   inherit (config.security) wrapperDir;
   fsPackagesPath = lib.makeBinPath config.system.fsPackages;
+
+  systemdWrapper = pkgs.writeShellScript "systemd-wrapper.sh" ''
+    mount -t binfmt_misc binfmt_misc /proc/sys/fs/binfmt_misc || true
+    exec systemd
+  '';
 }
