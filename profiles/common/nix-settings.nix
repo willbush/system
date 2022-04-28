d5867376cc206f65f1a454de822b6ff7e809b938
Will Bush <will.g.bush@gmail.com>
2022-04-28 17:17:37 -0500

    nixos: remove nix cache substituters that I don't use
    Former-commit-id: b7af7f841a66256465caa4463372d28547c4150f

--- profiles/common/nix-settings.nix
+++ profiles/common/nix-settings.nix
@@ -11,17 +11,13 @@
       substituters = [
         "https://cache.nixos.org/"
         "https://hercules-ci.cachix.org"
-        "https://hydra.iohk.io"
         "https://nix-community.cachix.org"
         "https://nix-tools.cachix.org"
-        "https://willbush.cachix.org"
       ];
       trusted-public-keys = [
         "hercules-ci.cachix.org-1:ZZeDl9Va+xe9j+KqdzoBZMFJHVQ42Uu/c/1/KMC5Lw0="
-        "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nix-tools.cachix.org-1:ebBEBZLogLxcCvipq2MTvuHlP7ZRdkazFSQsbs0Px1A="
-        "willbush.cachix.org-1:PuQjKarzPYTnxgEzKUoTDQ+aN0SImhO8NMZ0CamKBL4="
       ];
     };
     gc = {
