cd7fc32f4fe3dc48a9f8a94a7fc7fed8959f616e
Will Bush <will.g.bush@gmail.com>
2022-07-06 17:18:45 -0500

    nixos: enable dhcp

--- profiles/common/host-settings.nix
+++ profiles/common/host-settings.nix
@@ -1,10 +1,15 @@
+{ lib, ... }:
 {
   time.timeZone = "America/Chicago";
 
   sound.enable = true;
   hardware.pulseaudio.enable = true;
 
-  networking.useDHCP = false;
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
 
   # Change the default timeout for a service from 90 seconds.
   systemd.extraConfig = ''
