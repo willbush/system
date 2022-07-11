49cfb4711373051d0401c48848c4e5dfa013ce28
Will Bush <will.g.bush@gmail.com>
2022-07-11 00:59:35 -0500

    nixos: use pipewire for sound
    Former-commit-id: c1ce1d5823af20b1eb1b86538e52ce32a9956b10

--- profiles/common/host-settings.nix
+++ profiles/common/host-settings.nix
@@ -3,7 +3,14 @@
   time.timeZone = "America/Chicago";
 
   sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  # Enable sound with pipewire.
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
