a27aff30cc05092320f1952758066c04d42b9bb6
Will Bush <will.g.bush@gmail.com>
2022-07-11 00:48:34 -0500

    nixos: remove unnecessary settings

--- hosts/ton-618.nix
+++ hosts/ton-618.nix
@@ -24,14 +24,16 @@
   };
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/af889332-0e49-4822-adf8-4832ec485341";
+    {
+      device = "/dev/disk/by-uuid/af889332-0e49-4822-adf8-4832ec485341";
       fsType = "ext4";
     };
 
   boot.initrd.luks.devices."luks-d859f723-a2e7-41b9-ba03-af39da55e927".device = "/dev/disk/by-uuid/d859f723-a2e7-41b9-ba03-af39da55e927";
 
   fileSystems."/boot/efi" =
-    { device = "/dev/disk/by-uuid/136A-4599";
+    {
+      device = "/dev/disk/by-uuid/136A-4599";
       fsType = "vfat";
     };
 
@@ -42,9 +44,6 @@
 
   services = {
     xserver = {
-      layout = "us";
-      xkbVariant = "";
-
       videoDrivers = [ "amdgpu" ];
     };
     fstrim.enable = true;
