a0937c7684dde3c18187e47a840f72e82b9b2791
Will Bush <will.g.bush@gmail.com>
2022-07-12 15:41:58 -0500

    nixos: fix conflicting audio setting
    Former-commit-id: 23cc00b5ea4135f3b26a1618ad8114973cb587a2

--- profiles/common/host-settings.nix
+++ profiles/common/host-settings.nix
@@ -11,6 +11,7 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
+  hardware.pulseaudio.enable = false;
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
