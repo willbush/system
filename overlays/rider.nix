581995af368920bfab04c367e987b22784ec806d
Will Bush <will.g.bush@gmail.com>
2022-08-29 19:41:47 -0500

    nixos: add comments about Rider path to CLI tool
    Former-commit-id: 7ca007d74f0edc9cf01e34514a58a8983ec274a1

--- overlays/rider.nix
+++ overlays/rider.nix
@@ -3,6 +3,13 @@ self: super:
   jetbrains = super.jetbrains // {
     rider = super.jetbrains.rider.overrideAttrs (_: rec {
       # versions >= 2022.2 crash for me on startup. I haven't had time to look into why yet.
+      # Also note that for some reason Rider can't find .NET SDK, even with DOTNET_ROOT set.
+
+      # Therefore, in Rider, I have to do Settings > Build, Execution,
+      # Deployment > Tools and Build > .NET CLI executable path.
+      # and set it to something like:
+      # /nix/store/zzpcf8ysd51a82rfhbs1wpygi1hqi6wp-dotnet-sdk-6.0.400/dotnet
+      # exact path can be found with `dotnet --info`
       version = "2022.1.2";
       name = "rider-${version}";
 
