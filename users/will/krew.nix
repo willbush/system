ecfd5e45f7539f2fdb267db16c153da339ed522f
Will Bush <will.g.bush@gmail.com>
2022-10-19 14:50:18 -0500

    nixos: add comment about krew

--- users/will/krew.nix
+++ users/will/krew.nix
@@ -1,5 +1,7 @@
 { pkgs, ... }: {
   home = {
+    # Confusingly, this needs to be bootstrapped by executing `krew install
+    # krew` before `kubectl krew` works
     packages = with pkgs; [ krew ];
     sessionPath = [ "$HOME/.krew/bin" ];
   };
