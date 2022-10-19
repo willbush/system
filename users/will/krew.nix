b459b43ff6be499154884389c3317f2855d2e4f9
Will Bush <will.g.bush@gmail.com>
2022-10-19 15:00:56 -0500

    nixos: add another comment about krew plugins
    Former-commit-id: b2cb4e74cd72609d8316c91fe5ab51b822926336

--- users/will/krew.nix
+++ users/will/krew.nix
@@ -2,6 +2,8 @@
   home = {
     # Confusingly, this needs to be bootstrapped by executing `krew install
     # krew` before `kubectl krew` works
+    # Also, note that some plugins (such as rabbitmq) scripts need the interpreter changed to:
+    #!/usr/bin/env bash
     packages = with pkgs; [ krew ];
     sessionPath = [ "$HOME/.krew/bin" ];
   };
