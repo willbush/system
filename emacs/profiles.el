7b17d7ec03a47adc194ada32b092179a6c0597c3
Will Bush <will.g.bush@gmail.com>
2022-10-23 23:55:45 -0500

    emacs: add mutable chemacs2 profile

--- emacs/profiles.el
+++ emacs/profiles.el
@@ -1,5 +1,8 @@
 (
  ("default" . ((user-emacs-directory . "~/.config/emacs.default")))
+ ;; Mutable is for when I want to iterate fast on changes to my config without
+ ;; having to 'nixos-rebuild switch'.
+ ("mutable" . ((user-emacs-directory . "~/system/emacs/default")))
  ("rewrite" . ((user-emacs-directory . "~/.config/emacs.rewrite")))
  ("crafted" . ((user-emacs-directory . "~/.config/crafted-emacs")
                (env . (("CRAFTED_EMACS_HOME" . "~/.config/emacs.crafted")))))
