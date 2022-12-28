d69b58dd3008b2e0579b81e678ff76eab671be97
Will Bush <will.g.bush@gmail.com>
2022-12-27 21:40:00 -0600

    nixos: add explicit il8n internationalisation settings
    Former-commit-id: 8b148ad9313363282fc64fa0959913e218f0501a

--- profiles/common/host-settings.nix
+++ profiles/common/host-settings.nix
@@ -2,6 +2,21 @@
 {
   time.timeZone = "America/Chicago";
 
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
   sound.enable = true;
   # Enable sound with pipewire.
   security.rtkit.enable = true;
@@ -18,6 +33,7 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp6s0.useDHCP = lib.mkDefault true;
 
   # Change the default timeout for a service from 90 seconds.
   systemd.extraConfig = ''
