469d2921d9200d56495fce038165c72076e0cbc1
Will Bush <will.g.bush@gmail.com>
2023-01-03 02:12:31 -0600

    nixos: re-enable picom
    Former-commit-id: 1ba57f1d341c92f5518e472dca62afc5960c55dd

--- users/will/picom.nix
+++ users/will/picom.nix
@@ -3,12 +3,11 @@
     enable = true;
     fade = true;
     vSync = true;
-    experimentalBackends = true;
     # the default 'glx' backend lags like crazy for me for some reason.
     backend = "xrender";
     fadeDelta = 3;
     # I only want transparency for a couple of applications.
-    opacityRule = [
+    opacityRules = [
       "98:class_g *?= 'emacs' && focused"
       "88:class_g *?= 'emacs' && !focused"
       "98:class_g ?= 'alacritty' && focused"
