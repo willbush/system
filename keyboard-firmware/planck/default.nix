0829d96f35944cb43d209b8ca72a42c776ef3079
Will Bush <will.g.bush@gmail.com>
2023-01-06 02:17:07 -0600

    nixos: update planck firmware and simplify derivation
    Former-commit-id: d4d2b263dddb62e81d622c92d7ae19010bc2978f

--- keyboard-firmware/planck/default.nix
+++ keyboard-firmware/planck/default.nix
@@ -1,53 +1,39 @@
-{ fetchFromGitHub, dfu-programmer, dfu-util, gcc-arm-embedded-8, makeWrapper
-, python3, stdenv, writeScript }:
+{ stdenv, fetchFromGitHub, writeScript, qmk, makeWrapper }:
 
 let
   flash-planck = writeScript "flash-planck" ''
     #!/usr/bin/env bash
-
-    echo "Preparing to flash firmware in..."
-
-    for n in $(seq 5 -1 1);
-    do
-      echo $n
-      sleep 1
-    done
-
-    echo "Flashing..."
-
-    # Flashing is typically done using make in the qmk_firmware source directory.
-    # The make file (qmk_firmware/tmk_core/chibios.mk) has rules that call the
-    # dfu-util. I just wrapped the command with echo to see the arguments it was
-    # passing to dfu-util and did "make planck/rev6:willbush:dfu-util" to kick
-    # off the typical make build and flash command.
-
-    ${dfu-util}/bin/dfu-util -d 0483:df11 -a 0 -s 0x08000000:leave -D $1/planck_rev6_willbush.bin -v
+    cd $1
+    ${qmk}/bin/qmk flash ./.build/planck_rev6_willbush.bin
   '';
-in stdenv.mkDerivation rec {
+in
+stdenv.mkDerivation rec {
   name = "planck-rev6-firmware";
 
   src = fetchFromGitHub {
     owner = "qmk";
     repo = "qmk_firmware";
-    rev = "10e4487ba368a9e220c96e12a0d17ae729d5c1e1";
-    sha256 = "04r8pvgj6zv88h27c76lb5fvj7x3ahhfjc5kimqzbs8sikm5dfs3";
-    # date = 2020-12-13T03:19:15+09:00;
+    rev = "4a7d65b9d74af40fd5f92b58aad250f33f1af86a";
+    sha256 = "1azdbg16xbvz08392nkwx3pp597djb7xr7xn8x0xjj123jknqbfr";
+    # date = 2023-01-05T23:40:53+00:00;
     fetchSubmodules = true;
   };
 
-  buildInputs =
-    [ dfu-programmer dfu-util gcc-arm-embedded-8 python3 makeWrapper ];
+  buildInputs = [ qmk makeWrapper ];
 
   prePatch = ''
     mkdir -pv keyboards/planck/keymaps/willbush
     cp -rv ${./keymap}/* keyboards/planck/keymaps/willbush
   '';
 
-  buildPhase = "make planck/rev6:willbush";
+  # Use make instead of qmk tool so we can skip try to use git to determin
+  # firmware version.
+  buildPhase = "make SKIP_GIT=yes planck/rev6:willbush";
 
   installPhase = ''
     mkdir -p $out/bin
-    cp .build/planck_rev6_willbush.bin $out/
+    # qmk flash subcommand only becomes available when it's inside of qmk_firmware
+    cp -r . $out/
     # Wrap the script in order to pass the out path to it.
     makeWrapper ${flash-planck} $out/bin/flash-planck --add-flags $out
   '';
