7a16677ce115d0aea247da1fefc3dbb674d39c85
Will Bush <will.g.bush@gmail.com>
2023-02-13 20:57:09 -0600

    planck: remove gaming layer

--- keyboard-firmware/planck/keymap/keymap.c
+++ keyboard-firmware/planck/keymap/keymap.c
@@ -7,7 +7,6 @@
 enum planck_layers {
   _COLEMAK,
   _QWERTY,
-  _GAMING,
   _NUM,
   _LOWER,
   _RAISE,
@@ -17,8 +16,7 @@ enum planck_layers {
 
 enum planck_keycodes {
   COLEMAK = SAFE_RANGE,
-  QWERTY,
-  GAMING
+  QWERTY
 };
 
 #define LOWER MO(_LOWER)
@@ -43,12 +41,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT,
     _______, NUM,     KC_LGUI, KC_LALT, LOWER,   HYPER,   KC_SPC,  RAISE,   KC_RALT, KC_RGUI, _______, KC_RCTL
-  ),
-  [_GAMING] = LAYOUT_planck_grid(
-    KC_6,    KC_3,    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_BSPC,
-    KC_5,    KC_2,    CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_QUOT,
-    KC_4,    KC_1,    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, SFT_ENT,
-    COLEMAK, KC_DOT,  KC_LCTL, KC_SLSH, KC_LALT, KC_F1,   KC_SPC,  KC_F2,   KC_F3,   KC_F4,   KC_F5,   COLEMAK
   ),
   [_LOWER] = LAYOUT_planck_grid(
     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
@@ -76,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_ADJUST] = LAYOUT_planck_grid(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  _______, _______, COLEMAK, QWERTY,  GAMING,  _______, _______,
+    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  _______, _______, COLEMAK, QWERTY,  _______, _______, _______,
     _______, _______, _______, MU_ON,   MU_OFF,  _______, _______, _______, _______, _______, DB_TOGG, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT, _______
   )
@@ -97,12 +89,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case COLEMAK:
       if (record->event.pressed) {
         set_single_persistent_default_layer(_COLEMAK);
-      }
-      return false;
-      break;
-    case GAMING:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_GAMING);
       }
       return false;
       break;
