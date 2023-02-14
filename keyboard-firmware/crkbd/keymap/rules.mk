a2c0d50cd092bc3fb8d7ee408c35da2dcfd9f7f2
Will Bush <will.g.bush@gmail.com>
2023-02-13 20:36:06 -0600

    nixos: add corne (crkbd) firmware qmk config
    Former-commit-id: cf3fc76c2eaf449284c12675059fd6bfab8d0aa9

---
+++ keyboard-firmware/crkbd/keymap/rules.mk
@@ -0,0 +1,6 @@
+LTO_ENABLE        = yes
+MOUSEKEY_ENABLE   = yes    # Mouse keys
+OLED_DRIVER       = SSD1306
+OLED_ENABLE       = yes
+RGBLIGHT_ENABLE   = no     # Enable WS2812 RGB underlight.
+RGB_MATRIX_ENABLE = yes
