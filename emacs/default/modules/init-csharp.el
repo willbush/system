ee64c6a29222774d6e219d763bdf707755446b23
Will Bush <will.g.bush@gmail.com>
2023-03-15 15:18:47 -0500

    emacs: use nxml-mode for csproj and xaml files
    Former-commit-id: f53fd30a76e46450bc51efa1e732f1cfc6368db8

--- emacs/default/modules/init-csharp.el
+++ emacs/default/modules/init-csharp.el
@@ -14,4 +14,13 @@
                     c-basic-offset 4)
               (c-set-offset 'substatement-open 0))))
 
+(add-to-list 'auto-mode-alist '("\\.csproj\\'" . nxml-mode))
+(add-to-list 'auto-mode-alist '("\\.xaml\\'" . nxml-mode))
+
+(add-hook 'nxml-mode-hook
+          (lambda ()
+            (when (or (string-match "\\.csproj\\'" (buffer-file-name))
+                      (string-match "\\.xaml\\'" (buffer-file-name)))
+              (setq indent-tabs-mode t))))
+
 (provide 'init-csharp)
