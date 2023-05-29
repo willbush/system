400b0ff732146b684c373ab371c8c4df9e30c70f
Will Bush <will.g.bush@gmail.com>
2023-05-29 18:16:15 -0500

    nixos: retire tau-ceti
    Former-commit-id: e5572ed4557203c13047d5cf75bac34a26956c36

--- users/will/default.nix
+++ users/will/default.nix
@@ -30,9 +30,6 @@ in
           "tty"
           "dialout"
         ];
-        openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEE2hQsuOQZ3PvM2DdI0vxpaBFoRQpFhGXZmeRq8Srs6 tau-ceti-2020-11-16"
-        ];
       };
     };
   };
