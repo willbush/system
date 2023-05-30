dd394a5e37a7e9aa140c1ed67062f44131225bcb
Will Bush <will.g.bush@gmail.com>
2023-05-29 20:42:22 -0500

    keyboard: group rlues by yes/no and sort

--- keyboard-firmware/planck/keymap/rules.mk
+++ keyboard-firmware/planck/keymap/rules.mk
@@ -1,10 +1,10 @@
-AUDIO_ENABLE = yes    # Audio output
-COMMAND_ENABLE = yes  # Commands for debug and configuration
-NKRO_ENABLE = yes     # Enable N-Key Rollover
+AUDIO_ENABLE     = yes # Audio output
+COMMAND_ENABLE   = yes # Commands for debug and configuration
+NKRO_ENABLE      = yes # Enable N-Key Rollover
 
-STENO_ENABLE = no
-MOUSEKEY_ENABLE = no  # Mouse keys
-EXTRAKEY_ENABLE = no  # Audio control and System control
-CONSOLE_ENABLE = no   # Console for debug
 BACKLIGHT_ENABLE = no # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no  # Enable WS2812 RGB underlight.
+CONSOLE_ENABLE   = no # Console for debug
+EXTRAKEY_ENABLE  = no # Audio control and System control
+MOUSEKEY_ENABLE  = no # Mouse keys
+RGBLIGHT_ENABLE  = no # Enable WS2812 RGB underlight.
+STENO_ENABLE     = no
