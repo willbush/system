74af1cad3194274574344618634312364628f7fd
Will Bush <will.g.bush@gmail.com>
2023-05-29 20:42:22 -0500

    keyboard: group rlues by yes/no and sort
    Former-commit-id: dd394a5e37a7e9aa140c1ed67062f44131225bcb

--- keyboard-firmware/crkbd/keymap/rules.mk
+++ keyboard-firmware/crkbd/keymap/rules.mk
@@ -1,6 +1,7 @@
+MOUSEKEY_ENABLE   = no # Mouse keys
+RGBLIGHT_ENABLE   = no # Enable WS2812 RGB underlight.
+
 LTO_ENABLE        = yes
-MOUSEKEY_ENABLE   = yes    # Mouse keys
 OLED_DRIVER       = SSD1306
 OLED_ENABLE       = yes
-RGBLIGHT_ENABLE   = no     # Enable WS2812 RGB underlight.
 RGB_MATRIX_ENABLE = yes
