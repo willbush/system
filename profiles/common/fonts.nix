609da047e006ae08ba576331f4bce0176eff9a63
Will Bush <will.g.bush@gmail.com>
2023-05-30 18:10:51 -0500

    nixos: doom mode line now uses nerd fonts

--- profiles/common/fonts.nix
+++ profiles/common/fonts.nix
@@ -2,13 +2,13 @@
   fonts = {
     fonts = with pkgs; [
       dejavu_fonts
-      emacs-all-the-icons-fonts
       fira-mono
       hack-font
       inconsolata
       iosevka
       liberation_ttf
       libre-baskerville
+      nerdfonts
       powerline-fonts
       source-code-pro
       ubuntu_font_family
