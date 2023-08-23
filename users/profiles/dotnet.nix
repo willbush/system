e88d122745e2c74210feaebe6df6aaee16a454ce
Will Bush <will.g.bush@gmail.com>
2023-08-22 19:22:10 -0500

    nixos: used combined dotnet sdk 6 and 7

--- users/profiles/dotnet.nix
+++ users/profiles/dotnet.nix
@@ -1,17 +1,21 @@
 { pkgs, ... }:
-let sdk = pkgs.dotnet-sdk_7;
+let
+  combined_sdk = (with pkgs.dotnetCorePackages; combinePackages [
+    sdk_7_0
+    sdk_6_0
+  ]);
 in
 {
   home = {
     packages = [
       # note that Rider needs it's path to the dotnet cli tool fixed every time
       # this updates.
-      sdk
+      combined_sdk
     ];
     sessionVariables = {
       DOTNET_CLI_TELEMETRY_OPTOUT = 1;
-      # I thought this would help Rider found dotnet, but it doesn't seem to work.
-      DOTNET_ROOT = sdk;
+      # # I thought this would help Rider found dotnet, but it doesn't seem to work.
+      DOTNET_ROOT = combined_sdk;
     };
 
     # Add dotnet tools to path
