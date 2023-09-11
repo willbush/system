5498cc5ec47800a12c31e30c7de7e3608b83e3c1
Will Bush <will.g.bush@gmail.com>
2023-09-10 23:46:25 -0500

    nixos: follow nixpkgs in emacs-overlay input
    Former-commit-id: 2b418dc801039461483789b35d53aae22ebe0d44

--- flake.nix
+++ flake.nix
@@ -11,7 +11,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    emacs-overlay.url = "github:nix-community/emacs-overlay";
+    emacs-overlay = {
+      url = "github:nix-community/emacs-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     chemacs = {
       url = "github:plexus/chemacs2";
