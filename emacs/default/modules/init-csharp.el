5c9ef9ecbdf1c799e9386696a2eacc8b9efe230d
Will Bush <will.g.bush@gmail.com>
2023-11-17 17:47:47 -0600

    emacs: fix csharp-mode hook for tree-sitter
    Former-commit-id: 19c1d31f714a1957b0259c8b77e235de357f798d

--- emacs/default/modules/init-csharp.el
+++ emacs/default/modules/init-csharp.el
@@ -3,17 +3,21 @@
 (use-package csharp-mode
   :ensure nil ;; included in Emacs.
   :init
-  (add-hook 'csharp-mode-hook
-            (lambda ()
-              (setq indent-tabs-mode t
-                    c-syntactic-indentation t
-                    fill-column 100
-                    truncate-lines t
-                    tab-width 4
-                    evil-shift-width 4
-                    c-basic-offset 4)
-              (c-set-offset 'substatement-open 0)))
-  :config
+
+  (defun csharp-mode-setup ()
+    (setq indent-tabs-mode t
+          c-syntactic-indentation t
+          fill-column 100
+          truncate-lines t
+          tab-width 4
+          evil-shift-width 4
+          c-basic-offset 4)
+
+    (c-set-offset 'substatement-open 0))
+
+  (add-hook 'csharp-mode-hook 'csharp-mode-setup)
+  (add-hook 'csharp-ts-mode-hook 'csharp-mode-setup)
+
   (add-hook 'web-mode-hook
             (lambda ()
               (when (or (string-match "\\.csproj\\'" (buffer-file-name))
