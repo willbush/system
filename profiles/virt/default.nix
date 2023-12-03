7268300478cb4ac58472ff92cf64a667281756d6
Will Bush <will.g.bush@gmail.com>
2023-12-03 17:20:47 -0600

    nixos: update virt-manager to recommended install after 23.11

--- profiles/virt/default.nix
+++ profiles/virt/default.nix
@@ -8,13 +8,9 @@
     docker.enable = true;
   };
 
-  # virt-manager has a dependency on dconf.
-  # see: https://nixos.wiki/wiki/Virt-manager
-  # https://github.com/NixOS/nixpkgs/issues/52777
-  programs.dconf.enable = true;
+  programs.virt-manager.enable = true;
 
   environment.systemPackages = with pkgs; [
-    virt-manager # Add 'libvirtd' your user's extraGroups
     docker # Add 'docker' your user's extraGroups
     docker-compose
   ];
