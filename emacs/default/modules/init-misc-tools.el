a926b18dd2593b3a24ff791c6df301025f3ddf56
Will Bush <will.g.bush@gmail.com>
2023-12-12 19:42:11 -0600

    emacs: change gptel-model
    Former-commit-id: eb3f4be46497706cb362d91a1c1d8dfc0e86110e

--- emacs/default/modules/init-misc-tools.el
+++ emacs/default/modules/init-misc-tools.el
@@ -266,6 +266,7 @@
   (require 'gptel-curl)
 
   (setq gptel-default-mode 'org-mode)
+  (setq-default gptel-model "gpt-4-32k")
   (setq gptel-api-key
         (lambda ()
           (nth 4 (process-lines "gopass" "show" "will/work/openai.com")))))
