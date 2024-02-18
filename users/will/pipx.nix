10f8536d692ac1e78e595d52db64cff9604a61d5
Will Bush <will.g.bush@gmail.com>
2024-02-17 21:18:01 -0600

    nixos: delete pipx
    Former-commit-id: 04835fb6a2b042848cd7fb330e59fb227000df3d

--- users/will/pipx.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, ... }: {
-  home = {
-    # Only use pipx to install python apps that are not available in nixpkgs.
-    packages = with pkgs; [ pipx ];
-    sessionPath = [ "$HOME/.local/bin" ];
-  };
-}
