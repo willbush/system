21525797b3d7288aab1b884c0e696090857fdd5c
Will Bush <will.g.bush@gmail.com>
2024-02-18 01:36:42 -0600

    nixos: change greetd tty so boot output doesn't conflict with tui

--- users/will/greetd.nix
+++ users/will/greetd.nix
@@ -2,6 +2,7 @@
 {
   services.greetd = {
     enable = true;
+    vt = 2; # The virtual console (tty) that greetd should use.
     settings = {
       default_session.command = ''
         ${pkgs.greetd.tuigreet}/bin/tuigreet \
