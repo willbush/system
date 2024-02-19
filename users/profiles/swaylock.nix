180067a9494209bb0b33d8e4fd2d3d93016380e1
Will Bush <will.g.bush@gmail.com>
2024-02-18 23:56:29 -0600

    nixos: add swaylock
    Former-commit-id: e24390a64722b2c4f4a7392a9826de5b6f441dcd

---
+++ users/profiles/swaylock.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }: {
+  # requires `security.pam.services.swaylock = { };` at the system level or else
+  # unlock will not work.
+  programs.swaylock = {
+    enable = true;
+    package = pkgs.swaylock-effects;
+    settings = {
+      indicator-radius = 100;
+      indicator-thickness = 7;
+      text-color = "eaebf1";
+      effect-blur = "7x5";
+      effect-vignette = "0.5:0.5";
+      ring-color = "363b74";
+      key-hl-color = "673888";
+      line-color = "00000000";
+      inside-color = "05050b";
+      separator-color = "00000000";
+      grace = 2;
+      fade-in = 0.2;
+    };
+  };
+}
