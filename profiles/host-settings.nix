80dafe412357aca0fe866fd6676aeefa2f221caa
Will Bush <will.g.bush@gmail.com>
2024-02-23 17:25:37 -0600

    nixos: enable jack for pipewire
    Former-commit-id: 7468f1c8cbc177bd42dcec6c5aefe000a123b0f3

--- profiles/host-settings.nix
+++ profiles/host-settings.nix
@@ -22,8 +22,10 @@
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
+
     alsa.enable = true;
     alsa.support32Bit = true;
+    jack.enable = true;
     pulse.enable = true;
     wireplumber.enable = true;
   };
