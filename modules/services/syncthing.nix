a4bbc3002124c793fa0d131365790b6294aa5e7a
Will Bush <will.g.bush@gmail.com>
2024-02-28 19:56:41 -0600

    nixos: open ports for syncthing sync traffic / discovery
    Former-commit-id: e6cfeee477eee3cc5e746929fdad1c1183677f4d

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -25,6 +25,13 @@ in
 
     home-manager.users."${cfg.user}".services.syncthing.enable = true;
 
+    # Syncthing ports: 8384 for remote access to GUI
+    # 22000 TCP and/or UDP for sync traffic
+    # 21027/UDP for discovery
+    # source: https://docs.syncthing.net/users/firewall.html
+    networking.firewall.allowedTCPPorts = [ 22000 ];
+    networking.firewall.allowedUDPPorts = [ 22000 21027 ];
+
     services.syncthing = {
       enable = true;
       # Use home-manager service above. Syncthing is kinda slow to start and I
