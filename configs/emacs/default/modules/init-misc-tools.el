07f8276de63d57f4965bc24b4899a2db8ab70106
Will Bush <will.g.bush@gmail.com>
2024-03-02 23:09:02 -0600

    emacs: reorganize some code
    Former-commit-id: 3d1e83c46f33d8e06cf061381f6913f082aa66df

--- configs/emacs/default/modules/init-misc-tools.el
+++ configs/emacs/default/modules/init-misc-tools.el
@@ -284,4 +284,19 @@
    keycast-log-mode
    keycast-tab-bar-mode))
 
+(use-package zoom
+  :commands zoom zoom-mode
+  :config
+  (custom-set-variables
+   ;; golden ratio
+  '(zoom-size '(0.618 . 0.618))))
+
+(use-package adaptive-wrap
+  :commands adaptive-wrap-prefix-mode)
+
+(use-package visual-fill-column
+  :commands visual-fill-column-mode)
+
+(use-package sudo-edit :commands sudo-edit)
+
 (provide 'init-misc-tools)
