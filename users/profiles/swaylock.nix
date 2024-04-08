ba83398b6e9f5d925beab3d889c0a5c618f358b4
Will Bush <will.g.bush@gmail.com>
2024-04-07 20:21:28 -0500

    nixos: use catppunccin theme for swaylock
    Former-commit-id: 4c1561faf84b2c6682af7804cc24c8eaf2fe0038

--- users/profiles/swaylock.nix
+++ users/profiles/swaylock.nix
@@ -5,17 +5,13 @@
   programs.swaylock = {
     enable = true;
     package = pkgs.swaylock-effects;
+    catppuccin.enable = true;
+
     settings = {
       indicator-radius = 100;
       indicator-thickness = 7;
-      text-color = "eaebf1";
       effect-blur = "7x5";
       effect-vignette = "0.5:0.5";
-      ring-color = "363b74";
-      key-hl-color = "673888";
-      line-color = "00000000";
-      inside-color = "05050b";
-      separator-color = "00000000";
       grace = 2;
       fade-in = 0.2;
     };
