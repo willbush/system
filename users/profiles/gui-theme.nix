f200000340cac2a301fac59d8fc94d0aa755dccf
Will Bush <will.g.bush@gmail.com>
2024-04-07 20:24:48 -0500

    nixos: gtk use catppuccin theme
    Former-commit-id: 4616904a3ca121377daa5be1b67e7ea7f6be31b7

--- users/profiles/gui-theme.nix
+++ users/profiles/gui-theme.nix
@@ -9,12 +9,7 @@
 
   gtk = {
     enable = true;
-
-    theme.name = "rose-pine";
-    theme.package = pkgs.rose-pine-gtk-theme;
-
-    iconTheme.name = "rose-pine";
-    iconTheme.package = pkgs.rose-pine-icon-theme;
+    catppuccin.enable = true;
   };
 
   qt = {
