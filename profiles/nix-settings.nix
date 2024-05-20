5be1d6a078513fb51e9c1f222a7519d7e73ff716
Will Bush <git@willbush.dev>
2024-05-19 23:22:53 -0500

    nixos: switch to using optimise.automatic = true
    Former-commit-id: 1d1fb5b8adf2bcf09e02c161d1e0015fe0a70d64

--- profiles/nix-settings.nix
+++ profiles/nix-settings.nix
@@ -1,9 +1,14 @@
 { ... }:
 {
   nix = {
+    # Automatically run the nix store optimiser at a specific time. "If the
+    # system is off during the expected execution time, the timer is executed
+    # once the system is running again." The other option `auto-optimise-store =
+    # true` runs optimise on every build, which in theory has some overhead.
+    optimise.automatic = true;
+
     settings = {
       experimental-features = "nix-command flakes repl-flake";
-      auto-optimise-store = true;
       substituters = [ "https://nix-community.cachix.org" ];
       trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
     };
