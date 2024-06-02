99aeda7eb8b0a504ebbce7d4c4f7bd8ed7fb59e4
Will Bush <git@willbush.dev>
2024-06-02 01:33:10 -0500

    nixos: combine the two cli / gui nix files into one

--- users/will/pkgs/cli.nix
+++ users/will/pkgs/cli.nix
@@ -5,6 +5,34 @@ let
 in
 {
   home.packages = with pkgs; [
+    # core
+    curl
+    eza
+    fd
+    ripgrep
+    tree
+    wget
+
+    # dev
+    gh
+    nixfmt-rfc-style
+    nodePackages.prettier
+
+    # security
+    git-crypt
+    gopass
+
+    # compressor / archiver packages
+    unar
+    unzip
+    zip
+
+    # other utils
+    du-dust
+    tealdeer # tldr in Rust
+    trash-cli
+    usbutils
+
     # dev
     gcc # for `treesit-auto-install-all` which builds treesitter grammars
     gdb
--- users/will/pkgs/gui.nix
+++ users/will/pkgs/gui.nix
@@ -1,6 +1,31 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
+    # office
+    libreoffice-fresh
+    hunspellDicts.en_US-large # spellcheck dictionary used by libreoffice
+    simple-scan
+
+    # security
+    globalprotect-openconnect
+    keepassxc
+
+    # chat
+    signal-desktop
+
+    # video
+    vlc
+
+    # screenshot / screen capture
+    grim
+    slurp
+    obs-studio
+    peek
+
+    # image manipulation
+    inkscape
+    gimp
+
     # browsers
     (firefox.override { nativeMessagingHosts = [ browserpass ]; })
 
