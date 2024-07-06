558d5183f0c29052f3332f63e5a6ec44dacb6a85
Will Bush <git@willbush.dev>
2024-07-05 22:09:30 -0500

    nixos: use latest nix version
    Former-commit-id: ac47f94de13c4d68d7ead0eba34cb2c9d9210388

--- profiles/nixos/nix-settings.nix
+++ profiles/nixos/nix-settings.nix
@@ -1,6 +1,8 @@
-{ ... }:
+{ pkgs, ... }:
 {
   nix = {
+    package = pkgs.nixVersions.latest;
+
     # Automatically run the nix store optimiser at a specific time. "If the
     # system is off during the expected execution time, the timer is executed
     # once the system is running again." The other option `auto-optimise-store =
@@ -8,7 +10,7 @@
     optimise.automatic = true;
 
     settings = {
-      experimental-features = "nix-command flakes repl-flake";
+      experimental-features = "nix-command flakes";
       substituters = [ "https://nix-community.cachix.org" ];
       trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
     };
