1feab07b15e87892e2f221c392ca31c3ad7daefb
Will Bush <git@willbush.dev>
2024-07-05 22:09:06 -0500

    nixos: fix hardware.opengl renamed to hardware.graphics

--- profiles/nixos/host-settings.nix
+++ profiles/nixos/host-settings.nix
@@ -34,7 +34,7 @@
   };
   # needed for wayland
   security.polkit.enable = true;
-  hardware.opengl.enable = true;
+  hardware.graphics.enable = true;
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
