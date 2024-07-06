832dee36e4eef724102caa8c3ea1879cb887a304
Will Bush <git@willbush.dev>
2024-07-05 22:10:39 -0500

    nixos: fix gui theme with catppucin
    GTK needed to be abled explicitly in the theme.
    I just removed qt. I might add it back later. Not aware of any QT gui apps I use.

--- profiles/home/gui-theme.nix
+++ profiles/home/gui-theme.nix
@@ -1,20 +1,13 @@
 { pkgs, ... }:
 {
-
   home.pointerCursor = {
     gtk.enable = true;
     package = pkgs.vanilla-dmz;
     name = "Vanilla-DMZ";
   };
 
-  gtk.enable = true;
-
-  qt = {
+  gtk = {
     enable = true;
-    platformTheme.name = "gtk";
-    style = {
-      package = pkgs.adwaita-qt;
-      name = "adwaita-dark";
-    };
+    catppuccin.enable = true;
   };
 }
