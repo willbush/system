8b73289f6c0aeeb779912f233ff59c6b99625b78
Will Bush <git@willbush.dev>
2024-07-20 07:15:43 -0500

    nixos: add "nix cli helper" nh

--- profiles/nixos/nix-settings.nix
+++ profiles/nixos/nix-settings.nix
@@ -14,10 +14,5 @@
       substituters = [ "https://nix-community.cachix.org" ];
       trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
     };
-    gc = {
-      automatic = true;
-      dates = "23:00";
-      options = "--delete-older-than 30d";
-    };
   };
 }
