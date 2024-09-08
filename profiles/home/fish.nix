80badfaaf008dadc5272c4c40b6d92da83036310
Will Bush <git@willbush.dev>
2024-09-07 19:21:54 -0500

    nixos: disable fish greeting

--- profiles/home/fish.nix
+++ profiles/home/fish.nix
@@ -4,19 +4,15 @@
     fish = {
       enable = true;
 
+      interactiveShellInit = ''
+        set fish_greeting # Disable greeting
+      '';
+
       shellAliases = {
         k = "kubectl";
         la = "eza -lah";
         tp = "trash-put";
       };
-
-      # history = {
-      #   # path = "${config.xdg.dataHome}/zsh/zsh_history";
-      #   extended = false; # Whether to insert timestamps
-      #   ignoreDups = true;
-      #   size = 100000;
-      #   save = 100000;
-      # };
     };
     starship.enable = true;
   };
