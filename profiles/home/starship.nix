ea4c67d088394081fd091271a92b0d93e6e22f7b
Will Bush <git@willbush.dev>
2024-09-07 21:50:15 -0500

    starship: refactor
    Former-commit-id: f392e42f4ce45560cf622ea9990bb038936bdca8

--- profiles/home/starship.nix
+++ profiles/home/starship.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.starship = {
     enable = true;
@@ -6,6 +6,46 @@
       let
         format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
         style = "bg:#212736";
+        languages = {
+          aws = "";
+          buf = "";
+          c = "";
+          conda = "";
+          crystal = "";
+          dart = "";
+          docker_context = "";
+          elixir = "";
+          elm = "";
+          fennel = "";
+          fossil_branch = "";
+          git_branch = "";
+          golang = "";
+          guix_shell = "";
+          haskell = "";
+          haxe = "";
+          hg_branch = "";
+          java = "";
+          julia = "";
+          kotlin = "";
+          lua = "";
+          memory_usage = "󰍛";
+          meson = "󰔷";
+          nim = "󰆥";
+          nix_shell = "";
+          nodejs = "";
+          ocaml = "";
+          package = "󰏗";
+          perl = "";
+          php = "";
+          pijul_channel = "";
+          python = "";
+          rlang = "󰟔";
+          ruby = "";
+          rust = "󱘗";
+          scala = "";
+          swift = "";
+          zig = "";
+        };
       in
       {
         format = "[░▒▓](#a3aed2)[  ](bg:#a3aed2 fg:#090c0c)[](bg:#769ff0 fg:#a3aed2)$directory[](fg:#769ff0 bg:#394260)$git_branch$git_status[](fg:#394260 bg:#212736)$nodejs$rust$golang$php$gcloud$conda[](fg:#212736 bg:#1d2230)$time[ ](fg:#1d2230)\n$character";
@@ -16,11 +56,11 @@
           truncation_length = 3;
           truncation_symbol = "…/";
           substitutions = {
-            "documents" = "󰈙 ";
-            "downloads" = " ";
-            "music" = " ";
-            "pictures" = " ";
-            "images" = " ";
+            "documents" = "󰈙";
+            "downloads" = "";
+            "music" = "";
+            "pictures" = "";
+            "images" = "";
           };
         };
 
@@ -41,30 +81,10 @@
           style = "bg:#1d2230";
           format = "[[  $time ](fg:#a0a9cb bg:#1d2230)]($style)";
         };
-
-        nodejs = {
-          symbol = "";
-          inherit style;
-          inherit format;
-        };
-
-        rust = {
-          symbol = "";
-          inherit style;
-          inherit format;
-        };
-
-        golang = {
-          symbol = "";
-          inherit style;
-          inherit format;
-        };
-
-        php = {
-          symbol = "";
-          inherit style;
-          inherit format;
-        };
-      };
+      }
+      // pkgs.lib.attrsets.genAttrs (builtins.attrNames languages) (name: {
+        symbol = languages.${name};
+        inherit style format;
+      });
   };
 }
