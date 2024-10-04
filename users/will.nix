08ad1f9cb8d2acb42ceda9db660789d2d9b70ac2
Will Bush <git@willbush.dev>
2024-10-04 17:25:47 -0500

    nixos stylix: explicitly set font sizes

--- users/will.nix
+++ users/will.nix
@@ -172,6 +172,14 @@ in
     };
 
     fonts = {
+      # Explicitly setting this in-case I want to quickly increase size from the
+      # defaults.
+      sizes = {
+        applications = 12; # default: 12
+        desktop = 10; # default: 10
+        popups = 10; # default: 10
+        terminal = 12; # default: 12
+      };
       serif = {
         package = pkgs.dejavu_fonts;
         name = "DejaVu Serif";
