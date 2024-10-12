0e4bf92f92a070a820a8141cd0bcd3cca0e87d00
Will Bush <git@willbush.dev>
2024-10-12 16:57:06 -0500

    emacs: delete meow rewrite
    I like the idea of thin modal meow-edit on top of vanilla emacs, but thinking to
    move in the direction of using helix instead. Guess we'll see how that goes.

--- configs/emacs/profiles.el
+++ configs/emacs/profiles.el
@@ -2,5 +2,5 @@
  ("default" . ((user-emacs-directory . "~/.config/emacs.default")))
  ;; Mutable is for when I want to iterate fast on changes to my config without
  ;; having to 'nixos-rebuild switch'.
- ("mutable" . ((user-emacs-directory . "~/code/system/configs/emacs/meow")))
+ ("mutable" . ((user-emacs-directory . "~/code/system/configs/emacs/default")))
 )
