72579014e82917be1c6abe57b84a11344e924b69
Will Bush <git@willbush.dev>
2024-10-26 01:19:31 -0500

    nixos: remove k9s
    Former-commit-id: 43ad710f2717ec1ae070d156ddcea192fa337d04

--- profiles/home/programs.nix
+++ profiles/home/programs.nix
@@ -17,7 +17,6 @@
     fzf.enable = true;
     htop.enable = true;
     imv.enable = true; # command line image viewer intended for use with tiling window managers.
-    k9s.enable = true;
     wofi.enable = true;
     yazi.enable = true;
     zoxide.enable = true;
