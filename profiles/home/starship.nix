fcf360a8377c8ff059af32c52a3b72e168412b50
Will Bush <git@willbush.dev>
2024-11-03 00:41:19 -0500

    nixos: change starship preset to 'pure' and modify it a bit
    Former-commit-id: d1a48b4c1c6c6b762e27cc47a74fd30ba1af99c7

--- profiles/home/starship.nix
+++ profiles/home/starship.nix
@@ -2,89 +2,38 @@
 {
   programs.starship = {
     enable = true;
-    settings =
-      let
-        format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
-        style = "bg:#212736";
-        languages = {
-          aws = "";
-          buf = "";
-          c = "";
-          conda = "";
-          crystal = "";
-          dart = "";
-          docker_context = "";
-          elixir = "";
-          elm = "";
-          fennel = "";
-          fossil_branch = "";
-          git_branch = "";
-          golang = "";
-          guix_shell = "";
-          haskell = "";
-          haxe = "";
-          hg_branch = "";
-          java = "";
-          julia = "";
-          kotlin = "";
-          lua = "";
-          memory_usage = "󰍛";
-          meson = "󰔷";
-          nim = "󰆥";
-          nix_shell = "";
-          nodejs = "";
-          ocaml = "";
-          package = "󰏗";
-          perl = "";
-          php = "";
-          pijul_channel = "";
-          python = "";
-          rlang = "󰟔";
-          ruby = "";
-          rust = "󱘗";
-          scala = "";
-          swift = "";
-          zig = "";
-        };
-      in
-      {
-        format = "[░▒▓](#a3aed2)[  ](bg:#a3aed2 fg:#090c0c)[](bg:#769ff0 fg:#a3aed2)$directory[](fg:#769ff0 bg:#394260)$git_branch$git_status[](fg:#394260 bg:#212736)$nodejs$rust$golang$php$gcloud$conda[](fg:#212736 bg:#1d2230)$time[ ](fg:#1d2230)\n$character";
+    settings = {
+      format = "$directory$git_branch$git_state$git_status$line_break$character";
 
-        directory = {
-          style = "fg:#e3e5e5 bg:#769ff0";
-          format = "[ $path ]($style)";
-          truncation_length = 3;
-          truncation_symbol = "…/";
-          substitutions = {
-            "documents" = "󰈙";
-            "downloads" = "";
-            "music" = "";
-            "pictures" = "";
-            "images" = "";
-          };
-        };
+      directory.style = "blue";
 
-        git_branch = {
-          symbol = "";
-          style = "bg:#394260";
-          format = "[[ $symbol $branch ](fg:#769ff0 bg:#394260)]($style)";
-        };
+      character = {
+        success_symbol = "[❯](purple)";
+        error_symbol = "[❯](red)";
+        vimcmd_symbol = "[❮](green)";
+      };
 
-        git_status = {
-          style = "bg:#394260";
-          format = "[[($all_status$ahead_behind )](fg:#769ff0 bg:#394260)]($style)";
-        };
+      git_branch = {
+        format = "[$branch]($style)";
+        style = "bright-black";
+      };
 
-        time = {
-          disabled = false;
-          time_format = "%F %T";
-          style = "bg:#1d2230";
-          format = "[[  $time ](fg:#a0a9cb bg:#1d2230)]($style)";
-        };
-      }
-      // pkgs.lib.attrsets.genAttrs (builtins.attrNames languages) (name: {
-        symbol = languages.${name};
-        inherit style format;
-      });
+      git_status = {
+        format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
+        style = "cyan";
+        conflicted = "​";
+        untracked = "​";
+        modified = "​";
+        staged = "​";
+        renamed = "​";
+        deleted = "​";
+        stashed = "≡";
+      };
+
+      git_state = {
+        format = "\\([$state( $progress_current/$progress_total)]($style)\\) ";
+        style = "bright-black";
+      };
+    };
   };
 }
