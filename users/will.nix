3eacd627f2cc08eed9e8725dfe8b974575d5d525
Will Bush <git@willbush.dev>
2024-11-07 08:39:30 -0600

    Reapply "nixos: update"
    This reverts commit c779f172a9c7268c7e843467d242f9356f7dbdad.

--- users/will.nix
+++ users/will.nix
@@ -79,6 +79,8 @@ in
       enable = true;
       extraConfig = builtins.readFile ../configs/hypr/hyprland.conf;
     };
+    # TODO: remove this workaround https://github.com/danth/stylix/pull/605
+    stylix.targets.hyprland.enable = false;
 
     # lightweight notification daemon for Wayland
     services.mako = {
@@ -120,6 +122,7 @@ in
 
   programs.fish.enable = true;
   # important for system-wide configuration despite being installed via home-manager
+
   programs.hyprland.enable = true;
   programs.wireshark.enable = true;
 
