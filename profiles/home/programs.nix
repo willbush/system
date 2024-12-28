8e796d5336cbe323f6ec51cf17fa04174037aab2
Will Bush <git@willbush.dev>
2024-12-27 21:53:56 -0600

    nixos: replace fzf with skim
    meh why not

--- profiles/home/programs.nix
+++ profiles/home/programs.nix
@@ -10,7 +10,6 @@
     bat.enable = true;
     bottom.enable = true; # command: btm
     btop.enable = true;
-    fzf.enable = true;
     htop.enable = true;
     imv.enable = true; # command line image viewer intended for use with tiling window managers.
     rbw = {
@@ -34,6 +33,7 @@
         };
       });
     };
+    skim.enable = true;
     wofi.enable = true;
     yazi.enable = true;
     zoxide.enable = true;
