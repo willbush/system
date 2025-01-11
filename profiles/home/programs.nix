9cbc8cfceeb7cd61847d5856415aff92375a26ae
Will Bush <git@willbush.dev>
2025-01-10 19:45:01 -0600

    nixos: remove pin on rbw
    fixes todo

--- profiles/home/programs.nix
+++ profiles/home/programs.nix
@@ -12,27 +12,7 @@
     btop.enable = true;
     htop.enable = true;
     imv.enable = true; # command line image viewer intended for use with tiling window managers.
-    rbw = {
-      enable = true;
-      # TODO: remove after https://github.com/doy/rbw/issues/218 is merged
-      package = pkgs.rbw.overrideAttrs (_: {
-        src = pkgs.fetchFromGitHub {
-          owner = "davla";
-          repo = "rbw";
-          rev = "fix/client-name-header";
-          sha256 = "sha256-Sgs+qjKdtS5i7zF86TLSZMVKTDoeYhIgKEwjUUXw/cc=";
-        };
-        cargoDeps = pkgs.rustPlatform.importCargoLock {
-          lockFile = (
-            pkgs.fetchurl {
-              url = "https://raw.githubusercontent.com/davla/rbw/dd6b65427de3a4b38d27350d8ad7ebacb29e97ff/Cargo.lock";
-              hash = "sha256-bAELLBb0x0BOGPMLBRX/s0qxqN8XOxUW9OUa55WjeaA=";
-            }
-          );
-          allowBuiltinFetchGit = true;
-        };
-      });
-    };
+    rbw.enable = true;
     skim.enable = true;
     wofi.enable = true;
     yazi.enable = true;
