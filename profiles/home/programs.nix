7318f55de752a0c0e9a0ada42962b00a97cdd047
Will Bush <git@willbush.dev>
2025-01-11 23:25:18 -0600

    nixos: switch from alacritty to ghostty
    Was going to switch to wezterm, but its default keybindings that use super are
    too annoying and ghostty defaults are really good altough I unbind/rebind all
    the keys.

--- profiles/home/programs.nix
+++ profiles/home/programs.nix
@@ -6,26 +6,53 @@
       nix-direnv.enable = true;
     };
 
-    alacritty = {
+    ghostty = {
       enable = true;
       settings = {
-        keyboard.bindings = [
-          {
-            key = "Plus";
-            # alacritty thinks I'm pressing shift even though I'm not. Perhaps
-            # something to do with QMK firmware.
-            # https://github.com/alacritty/alacritty/issues/6496
-            mods = "Control | Shift";
-            action = "IncreaseFontSize";
-          }
-          {
-            key = "Minus";
-            mods = "Control";
-            action = "DecreaseFontSize";
-          }
+        auto-update = "off";
+        window-decoration = false;
+
+        # Keybindings
+        keybind = [
+          "clear" # clear all defaults
+
+          "ctrl+shift+comma=reload_config"
+
+          "ctrl+shift+d=toggle_window_decorations"
+
+          "ctrl+shift+physical:equal=increase_font_size:1" # C-+ handles QMK quirk
+          "ctrl+minus=decrease_font_size:1"
+          "ctrl+zero=reset_font_size"
+
+          "ctrl+shift+a=select_all"
+          "ctrl+shift+c=copy_to_clipboard"
+          "ctrl+shift+v=paste_from_clipboard"
+
+          "ctrl+alt+shift+j=write_scrollback_file:open"
+          "ctrl+shift+j=write_scrollback_file:paste"
+
+          "shift+end=scroll_to_bottom"
+          "shift+home=scroll_to_top"
+          "shift+page_down=scroll_page_down"
+          "shift+page_up=scroll_page_up"
+
+          # Tabs
+          "ctrl+shift+t=new_tab"
+          "ctrl+shift+tab=previous_tab"
+          "ctrl+shift+w=close_surface"
+          "ctrl+tab=next_tab"
+
+          # Splits (ctrl+a leader key)
+          "ctrl+a>s=new_split:down" # bottom split (vim like)
+          "ctrl+a>v=new_split:right" # vertical
+
+          "ctrl+shift+enter=toggle_split_zoom"
+          "ctrl+shift+n=goto_split:next"
+          "ctrl+shift+p=goto_split:previous"
         ];
       };
     };
+
     bat.enable = true;
     bottom.enable = true; # command: btm
     btop.enable = true;
