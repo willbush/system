01aa0c462c2a475dfdba58fa3c8f7229ca6c87eb
Will Bush <git@willbush.dev>
2025-03-16 03:59:44 -0500

    nixos: move theme related things to theme.nix

---
+++ profiles/mixed/theme.nix
@@ -0,0 +1,52 @@
+{ config, pkgs, ... }:
+{
+  stylix = {
+    enable = true;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
+    image = pkgs.fetchurl {
+      # https://www.reddit.com/r/WidescreenWallpaper/comments/1dzli4w/untitled_7680x2160/
+      url = "https://i.redd.it/1k3jwtm7zlbd1.jpeg";
+      hash = "sha256-3GJ6pwrExTGJ5y+X//7iBY2ABDUkcFlxQzot24evceo=";
+    };
+
+    fonts = {
+      # Explicitly setting this in-case I want to quickly increase size from the
+      # defaults.
+      sizes = {
+        applications = 14; # default: 12
+        desktop = 12; # default: 10
+        popups = 12; # default: 10
+        terminal = 14; # default: 12
+      };
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans";
+      };
+
+      monospace = {
+        package = pkgs.nerd-fonts.zed-mono;
+        name = "ZedMono NFM Extd";
+      };
+
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+    };
+  };
+
+  home-manager.users.${config.user.name} = {
+    home.pointerCursor = {
+      enable = true;
+      gtk.enable = true;
+      name = "BreezeX-RosePine-Linux";
+      package = pkgs.rose-pine-cursor;
+      size = 32;
+    };
+  };
+}
--- users/will.nix
+++ users/will.nix
@@ -13,6 +13,7 @@ in
     ../modules/services/docker.nix
     ../modules/services/lianli-pwm-rgb-sync.nix
     ../modules/services/virt.nix
+    ../profiles/mixed/theme.nix
     ../profiles/nixos/greetd.nix
     ../profiles/nixos/less.nix
   ];
@@ -104,14 +105,6 @@ in
 
     gtk.enable = true;
 
-    home.pointerCursor = {
-      enable = true;
-      gtk.enable = true;
-      name = "BreezeX-RosePine-Linux";
-      package = pkgs.rose-pine-cursor;
-      size = 32;
-    };
-
     wayland.windowManager.hyprland = {
       enable = true;
       extraConfig = builtins.readFile ../configs/hypr/hyprland.conf;
@@ -201,46 +194,6 @@ in
     noto-fonts-cjk-sans
     noto-fonts-emoji
   ];
-
-  stylix = {
-    enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
-    image = pkgs.fetchurl {
-      # https://www.reddit.com/r/WidescreenWallpaper/comments/1dzli4w/untitled_7680x2160/
-      url = "https://i.redd.it/1k3jwtm7zlbd1.jpeg";
-      hash = "sha256-3GJ6pwrExTGJ5y+X//7iBY2ABDUkcFlxQzot24evceo=";
-    };
-
-    fonts = {
-      # Explicitly setting this in-case I want to quickly increase size from the
-      # defaults.
-      sizes = {
-        applications = 14; # default: 12
-        desktop = 12; # default: 10
-        popups = 12; # default: 10
-        terminal = 14; # default: 12
-      };
-      serif = {
-        package = pkgs.dejavu_fonts;
-        name = "DejaVu Serif";
-      };
-
-      sansSerif = {
-        package = pkgs.dejavu_fonts;
-        name = "DejaVu Sans";
-      };
-
-      monospace = {
-        package = pkgs.nerd-fonts.zed-mono;
-        name = "ZedMono NFM Extd";
-      };
-
-      emoji = {
-        package = pkgs.noto-fonts-emoji;
-        name = "Noto Color Emoji";
-      };
-    };
-  };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "24.11";
