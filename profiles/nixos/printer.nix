f9242dfcc15814436b5eca66af478febe46aa2ea
Will Bush <git@willbush.dev>
2025-04-25 23:35:56 -0500

    nixos: fix / simplify printer setup

--- profiles/nixos/printer.nix
+++ profiles/nixos/printer.nix
@@ -1,38 +1,19 @@
+{ pkgs, ... }:
 {
-  hardware = {
-    sane.enable = true;
-    sane.brscan4 = {
-      enable = true;
-      netDevices = {
-        office-printer = {
-          model = "HL-2380DW";
-          ip = "192.168.68.58";
-        };
-      };
-    };
+  hardware.sane = {
+    enable = true;
+    extraBackends = [ pkgs.sane-airscan ];
   };
 
   services = {
-    # Note: first time setup requires going to http://localhost:631 and adding
-    # the printer to the cups service. Also, to get an lpd:// connection to the
-    # printer I have to temporarily disable the firewall because it seems to use
-    # a randomly assigned port when discovering the network printer. Enabling
-    # avahi gets around that issue by connecting via a different means.
-    printing = {
-      enable = true;
-      logLevel = "notice"; # one level below "info"
-    };
+    # NOTE: first time setup requires going to http://localhost:631 and adding
+    # the printer to the cups service.
+    printing.enable = true;
 
     avahi = {
       enable = true;
       nssmdns4 = true;
+      openFirewall = true;
     };
   };
-
-  modules.unfree.allowList = [
-    "hll2390dw-cups"
-    "brscan4"
-    "brother-udev-rule-type1"
-    "brscan4-etc-files"
-  ];
 }
