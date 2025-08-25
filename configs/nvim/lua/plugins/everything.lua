056e6d1aef1c3fc288cca37e05c0a7e6676bd9e9
Will Bush <git@willbush.dev>
2025-08-25 02:32:27 -0500

    nvim: compile all plugins into one file for now

--- configs/nvim/lua/plugins/everything.lua
+++ configs/nvim/lua/plugins/everything.lua
@@ -4,7 +4,7 @@ return {
     -- optional: provides snippets for the snippet source
     dependencies = 'rafamadriz/friendly-snippets',
 
-    version = '*',
+    version = '1.*',
 
     opts = {
       keymap = { preset = 'default' },
@@ -178,6 +178,189 @@ return {
       },
     },
   },
-}
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      bigfile = { enabled = false },
+      dashboard = { enabled = false },
+      indent = { enabled = false },
+      input = { enabled = false },
+      notifier = { enabled = false },
+      quickfile = { enabled = false },
+      scroll = { enabled = false },
+      statuscolumn = { enabled = false },
+      words = { enabled = false },
+    },
+  },
+  {
+    'folke/tokyonight.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      vim.cmd.colorscheme 'tokyonight-night'
 
--- vim: ts=2 sts=2 sw=2 et
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+  -- git signs highlights text that has changed since the list
+  -- git commit, and also lets you interactively stage & unstage
+  -- hunks in a commit.
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "VimEnter",
+    opts = {},
+  },
+
+  { 'echasnovski/mini.icons', lazy = true, version = false },
+  { 'echasnovski/mini.statusline', opts = {}, version = false },
+
+  -- { -- Collection of various small independent plugins/modules
+  --   'echasnovski/mini.nvim',
+  --   config = function()
+  --     local statusline = require 'mini.statusline'
+  --     statusline.setup { use_icons = vim.g.have_nerd_font }
+
+  --     -- You can configure sections in the statusline by overriding their
+  --     -- default behavior. For example, here we set the section for
+  --     -- cursor location to LINE:COLUMN
+  --     ---@diagnostic disable-next-line: duplicate-set-field
+  --     statusline.section_location = function()
+  --       return '%2l:%-2v'
+  --     end
+
+  --     -- ... and there is more!
+  --     --  Check out: https://github.com/echasnovski/mini.nvim
+  --   end,
+  -- },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+
+      plugins = {
+        presets = {
+          -- disable the built-in window commands since I remap many of them. It
+          -- will show the hjkl mappings even if I filter them out.
+          windows = false,
+        }
+      },
+
+      -- Exclude mappings with no description
+      filter = function(mapping)
+        return mapping.desc and mapping.desc ~= ""
+      end,
+
+      icons = {
+        -- set icon mappings to true if you have a Nerd Font
+        mappings = vim.g.have_nerd_font,
+      },
+      -- Document existing key chains
+      spec = {
+        {
+          mode = { "n", "v" },
+          -- { "<leader><tab>", group = "tabs" },
+          -- { "<leader>c", group = "code" },
+          -- { "<leader>d", group = "debug" },
+          -- { "<leader>dp", group = "profiler" },
+          -- { "<leader>g", group = "git" },
+          -- { "<leader>gh", group = "hunks" },
+          -- { "<leader>q", group = "quit/session" },
+          -- { "<leader>u", group = "ui", icon = { icon = "󰙵 ", color = "cyan" } },
+          -- { "<leader>x", group = "diagnostics/quickfix", icon = { icon = "󱖫 ", color = "green" } },
+          -- { "gs", group = "surround" },
+          { "<leader>f", group = "file" },
+          { "<leader>q", group = "quit" },
+          { "<leader>r", group = "rapid" },
+          { "<leader>s", group = "search" },
+          { "<leader>w", proxy = "<c-w>", group = "windows" }, -- proxy to window mappings
+          { "[", group = "prev" },
+          { "]", group = "next" },
+          { "g", group = "goto" },
+          { "z", group = "fold" },
+          {"<leader>b", group = "buffer" },
+        },
+      },
+    },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  },
+
+
+  { -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+    opts = {
+        ensure_installed = {
+          'bash',
+          'c',
+          'diff',
+          'html',
+          'lua',
+          'luadoc',
+          'markdown',
+          'markdown_inline',
+          'query',
+          'ron',
+          'rust',
+          'vim',
+          'vimdoc',
+        },
+      -- Autoinstall languages that are not installed
+      auto_install = true,
+      highlight = {
+        enable = true,
+        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+        --  If you are experiencing weird indenting issues, add the language to
+        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+        additional_vim_regex_highlighting = { 'ruby' },
+      },
+      indent = { enable = true, disable = { 'ruby' } },
+    },
+    -- There are additional nvim-treesitter modules that you can use to interact
+    -- with nvim-treesitter. You should go explore a few and see what interests you:
+    --
+    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+  },
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = { 'saghen/blink.cmp' },
+
+    opts = {
+      servers = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              completion = {
+                callSnippet = 'Replace',
+              },
+              diagnostics = { globals = { "vim" }, },
+            },
+          },
+        },
+        rust_analyzer = {},
+      }
+    },
+    config = function(_, opts)
+      local lspconfig = require('lspconfig')
+      for server, config in pairs(opts.servers) do
+        -- passing config.capabilities to blink.cmp merges with the capabilities in your
+        -- `opts[server].capabilities, if you've defined it
+        config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+        lspconfig[server].setup(config)
+      end
+    end
+  },
+}
