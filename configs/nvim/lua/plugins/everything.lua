11cdcc5bfe400d0d889cec6df56cb2b6b6df6012
Will Bush <git@willbush.dev>
2025-10-04 16:07:15 -0500

    nvim: remove old blink nvim-lspconfig

--- configs/nvim/lua/plugins/everything.lua
+++ configs/nvim/lua/plugins/everything.lua
@@ -428,33 +428,4 @@ return {
       },
     },
   },
-  {
-    'neovim/nvim-lspconfig',
-    dependencies = { 'saghen/blink.cmp' },
-
-    opts = {
-      servers = {
-        lua_ls = {
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-              diagnostics = { globals = { 'vim' } },
-            },
-          },
-        },
-        rust_analyzer = {},
-      },
-    },
-    config = function(_, opts)
-      local lspconfig = require('lspconfig')
-      for server, config in pairs(opts.servers) do
-        -- passing config.capabilities to blink.cmp merges with the capabilities in your
-        -- `opts[server].capabilities, if you've defined it
-        config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
-        lspconfig[server].setup(config)
-      end
-    end,
-  },
 }
