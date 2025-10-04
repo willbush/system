d167acbc1d0dc32b1afff621f1623ec6d6276016
Will Bush <git@willbush.dev>
2025-10-04 16:49:35 -0500

    add comment

--- configs/nvim/lua/plugins/everything.lua
+++ configs/nvim/lua/plugins/everything.lua
@@ -17,7 +17,7 @@ return {
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
     -- used for completion, annotations and signatures of Neovim apis
     'folke/lazydev.nvim',
-    ft = 'lua',
+    ft = 'lua', -- only load on lua files
     opts = {
       library = {
         -- Load luvit types when the `vim.uv` word is found
