2f652de38889b8340b615daf08b495bec63bfb4f
Will Bush <git@willbush.dev>
2025-10-08 04:55:34 -0500

    nvim: delete commented out mini.statusline
    will reconsider status line later

--- configs/nvim/lua/plugins/everything.lua
+++ configs/nvim/lua/plugins/everything.lua
@@ -213,23 +213,4 @@ return {
 
   { 'echasnovski/mini.icons', lazy = true, version = false },
   { 'echasnovski/mini.statusline', opts = {}, version = false },
-
-  -- { -- Collection of various small independent plugins/modules
-  --   'echasnovski/mini.nvim',
-  --   config = function()
-  --     local statusline = require 'mini.statusline'
-  --     statusline.setup { use_icons = vim.g.have_nerd_font }
-
-  --     -- You can configure sections in the statusline by overriding their
-  --     -- default behavior. For example, here we set the section for
-  --     -- cursor location to LINE:COLUMN
-  --     ---@diagnostic disable-next-line: duplicate-set-field
-  --     statusline.section_location = function()
-  --       return '%2l:%-2v'
-  --     end
-
-  --     -- ... and there is more!
-  --     --  Check out: https://github.com/echasnovski/mini.nvim
-  --   end,
-  -- },
 }
