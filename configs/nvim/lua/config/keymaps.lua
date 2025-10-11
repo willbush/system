501481be506eb00601c58610ab9fe2239ceff4c9
Will Bush <git@willbush.dev>
2025-10-11 18:54:06 -0500

    nvim: refactor / work on keymaps

--- configs/nvim/lua/config/keymaps.lua
+++ configs/nvim/lua/config/keymaps.lua
@@ -1,29 +1,67 @@
-local map = vim.keymap.set
+-- modes
+local n = { 'n' }
 
--- Note this maps over default `U` (see `:h U`) which "undo all latest changes
--- on one line." I fail to imagine ever wanting that.
-map('n', 'U', '<C-R>')
+local keymaps = {
+  -- Note this maps over default `U` (see `:h U`) which "undo all latest
+  -- changes on one line." I fail to imagine ever wanting that.
+  { lhs = 'U', rhs = '<C-R>', desc = 'Redo', modes = n },
+  { lhs = '<esc>', rhs = '<cmd>nohlsearch<cr>', desc = 'Clear search highlights', modes = n },
 
--- Clear highlights on search when pressing <Esc> in normal mode
---  See `:help hlsearch`
-map('n', '<Esc>', '<cmd>nohlsearch<CR>')
+  -- Exit terminal mode in the builtin terminal with a shortcut that is a bit
+  -- easier for people to discover. Otherwise, you normally need to press
+  -- <C-\><C-n>, which is not what someone will guess without a bit more
+  -- experience.
+  { lhs = '<esc><esc>', rhs = '<C-\\><C-n>', desc = 'Exit terminal mode', modes = { 't' } },
 
-map('n', '<leader>qq', '<cmd>wqa<CR>', { desc = 'Write and Quit all' })
+  -- Rapid (emphasize easy access over mnemonics)
+  { lhs = '<leader>rs', rhs = '<cmd>w<cr>', desc = 'Save buffer', modes = n },
 
--- Rapid (emphasize easy access over mnemonics)
-map('n', '<leader>rs', '<cmd>w<CR>', { desc = 'Save buffer' })
+  -- Buffers
+  {
+    lhs = '<leader>bb',
+    rhs = function()
+      require('fzf-lua').buffers()
+    end,
+    desc = 'Buffer picker',
+    modes = n,
+  },
+  {
+    lhs = '<leader>bd',
+    rhs = function()
+      require('snacks').bufdelete()
+    end,
+    desc = 'Delete Buffer',
+    modes = n,
+  },
+  {
+    lhs = '<leader>bk',
+    rhs = '<cmd>bd<cr>',
+    desc = 'Kill (delete) Buffer and Window',
+    modes = n,
+  },
+  {
+    lhs = '<leader>bD',
+    rhs = function()
+      require('snacks').bufdelete.all()
+    end,
+    desc = 'Delete All Buffers',
+    modes = n,
+  },
+  {
+    lhs = '<leader>bO',
+    rhs = function()
+      require('snacks').bufdelete.other()
+    end,
+    desc = 'Delete Other Buffers',
+    modes = n,
+  },
+}
 
--- Buffers
-map('n', '<leader>bd', function()
-  Snacks.bufdelete()
-end, { desc = 'Delete Buffer' })
-map('n', '<leader>bo', function()
-  Snacks.bufdelete.other()
-end, { desc = 'Delete Other Buffers' })
-
--- map("n", "<leader>bD", "<cmd>:bd<cr>", { desc = "Delete Buffer and Window" })
-
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
-map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+for _, ks in ipairs(keymaps) do
+  local opts = { desc = ks.desc }
+  -- Merge any extra options
+  if ks.opts then
+    opts = vim.tbl_extend('force', opts, ks.opts)
+  end
+  vim.keymap.set(ks.modes, ks.lhs, ks.rhs, opts)
+end
