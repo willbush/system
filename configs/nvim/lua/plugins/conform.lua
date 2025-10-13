37f8a86f44276b750fc2e8632f924e044c763b52
Will Bush <git@willbush.dev>
2025-10-12 23:17:00 -0500

    nvim: add rust and nix to conform

--- configs/nvim/lua/plugins/conform.lua
+++ configs/nvim/lua/plugins/conform.lua
@@ -9,6 +9,8 @@ return {
     opts = {
       formatters_by_ft = {
         lua = { 'stylua' },
+        nix = { 'nixfmt' },
+        rust = { 'rustfmt' },
       },
       format_on_save = { timeout_ms = 500 },
     },
