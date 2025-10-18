60d23044b0a7528b50a44ab11bbb7326bcfe4b82
Will Bush <git@willbush.dev>
2025-10-17 20:01:39 -0500

    nvim: configure fff.nvim more

--- configs/nvim/lua/plugins/fff.lua
+++ configs/nvim/lua/plugins/fff.lua
@@ -1,7 +1,13 @@
 return {
   'dmtrKovalenko/fff.nvim',
   build = 'nix run .#release',
-  opts = {},
+  opts = {
+    prompt = '❯ ',
+    max_threads = 8,
+    layout = {
+      prompt_position = 'top',
+    },
+  },
   -- This plugin initializes itself lazily.
   lazy = false,
 }
