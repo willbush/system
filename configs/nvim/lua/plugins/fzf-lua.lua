883cc90b712b42d36e53a51d243c79c3b7e10fda
Will Bush <git@willbush.dev>
2025-10-17 21:58:33 -0500

    nvim fzf-lua: configure register_ui_select

--- configs/nvim/lua/plugins/fzf-lua.lua
+++ configs/nvim/lua/plugins/fzf-lua.lua
@@ -3,11 +3,12 @@ return {
     'ibhagwan/fzf-lua',
     dependencies = { 'nvim-mini/mini.icons' },
     cmd = 'FzfLua',
-    opts = {
-      'skim', -- requires `sk` binary
-    },
     config = function()
-      require('fzf-lua').register_ui_select()
+      local fzf_lua = require('fzf-lua')
+      fzf_lua.setup({
+        'skim', -- requires `sk` binary
+      })
+      fzf_lua.register_ui_select()
     end,
   },
 }
