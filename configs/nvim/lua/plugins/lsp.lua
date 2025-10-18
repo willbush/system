d1908bd8bc09d848d5129daf45446af75b8c170e
Will Bush <git@willbush.dev>
2025-10-18 04:28:51 -0500

    nvim: bind some rustaceanvim keys

--- configs/nvim/lua/plugins/lsp.lua
+++ configs/nvim/lua/plugins/lsp.lua
@@ -25,5 +25,27 @@ return {
     'mrcjkb/rustaceanvim',
     version = '^6',
     lazy = false, -- This plugin is already lazy
+    config = function()
+      vim.g.rustaceanvim = {
+        server = {
+          on_attach = function(client, bufnr)
+            local function map(mode, lhs, rhs, opts)
+              opts = opts or {}
+              opts.buffer = bufnr
+              opts.silent = true
+              vim.keymap.set(mode, lhs, rhs, opts)
+            end
+
+            map('n', 'K', function()
+              vim.cmd.RustLsp({ 'hover', 'actions' })
+            end, { desc = 'Hover Actions (Rustaceanvim)' })
+            map('n', '<leader>a', function()
+              vim.cmd.RustLsp('codeAction')
+            end, { desc = 'Code Action (Rustaceanvim)' })
+            map('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to Definition' })
+          end,
+        },
+      }
+    end,
   },
 }
