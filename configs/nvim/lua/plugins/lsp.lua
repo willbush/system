b8b8ba98dd6711c5c8ccdd995755e99161dbae25
Will Bush <git@willbush.dev>
2025-10-18 04:10:54 -0500

    nvim: fix lsp settings..

--- configs/nvim/lua/plugins/lsp.lua
+++ configs/nvim/lua/plugins/lsp.lua
@@ -1,22 +1,5 @@
 return {
   {
-    'neovim/nvim-lspconfig',
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-      vim.lsp.enable('rust-analyzer')
-      vim.lsp.config('rust-analyzer', {
-        cmd = { 'rust-analyzer' },
-        filetypes = { 'rust' },
-      })
-
-      vim.lsp.enable('lua-language-server')
-      vim.lsp.config('lua-language-server', {
-        cmd = { 'lua-language-server' },
-        filetypes = { 'lua' },
-      })
-    end,
-  },
-  {
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
     -- used for completion, annotations and signatures of Neovim apis
     'folke/lazydev.nvim',
@@ -27,5 +10,15 @@ return {
         { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
       },
     },
+  },
+  {
+    'neovim/nvim-lspconfig',
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+      vim.lsp.enable({
+        'rust_analyzer',
+        'lua_ls',
+      })
+    end,
   },
 }
