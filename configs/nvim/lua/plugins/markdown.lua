ed3c58f0cac13de11a72c6478956f4946f650fe2
Will Bush <git@willbush.dev>
2025-11-14 22:31:17 -0600

    nvim: remove markdown-plus for now
    christ key maps in nvim are a pita

--- configs/nvim/lua/plugins/markdown.lua
+++ configs/nvim/lua/plugins/markdown.lua
@@ -7,37 +7,4 @@ return {
     ---@type render.md.UserConfig
     opts = {},
   },
-  {
-    'yousefhadder/markdown-plus.nvim',
-    ft = 'markdown',
-    opts = {
-      enabled = true,
-      features = {
-        list_management = true,
-        text_formatting = true,
-        headers_toc = true,
-        links = true,
-        quotes = true,
-        code_block = true,
-        table = true,
-      },
-      keymaps = {
-        enabled = true, -- Enable default keymaps (<Plug> available for custom)
-      },
-      toc = { -- TOC window configuration
-        initial_depth = 2,
-      },
-      table = {
-        auto_format = true,
-        default_alignment = 'left',
-        confirm_destructive = false,
-        keymaps = {
-          enabled = true,
-          prefix = '<leader>t',
-          insert_mode_navigation = false,
-        },
-      },
-      filetypes = { 'markdown' }, -- Filetypes to enable the plugin for
-    },
-  },
 }
