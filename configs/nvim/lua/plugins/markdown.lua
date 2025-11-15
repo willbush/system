5cebf56db45111e6cba72e055b006c7262aee46e
Will Bush <git@willbush.dev>
2025-11-14 21:13:10 -0600

    nvim: add markdown plus

--- configs/nvim/lua/plugins/markdown.lua
+++ configs/nvim/lua/plugins/markdown.lua
@@ -1,8 +1,43 @@
 return {
-  'MeanderingProgrammer/render-markdown.nvim',
-  ft = 'markdown',
-  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },
-  ---@module 'render-markdown'
-  ---@type render.md.UserConfig
-  opts = {},
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    ft = 'markdown',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+  {
+    'yousefhadder/markdown-plus.nvim',
+    ft = 'markdown',
+    opts = {
+      enabled = true,
+      features = {
+        list_management = true,
+        text_formatting = true,
+        headers_toc = true,
+        links = true,
+        quotes = true,
+        code_block = true,
+        table = true,
+      },
+      keymaps = {
+        enabled = true, -- Enable default keymaps (<Plug> available for custom)
+      },
+      toc = { -- TOC window configuration
+        initial_depth = 2,
+      },
+      table = {
+        auto_format = true,
+        default_alignment = 'left',
+        confirm_destructive = false,
+        keymaps = {
+          enabled = true,
+          prefix = '<leader>t',
+          insert_mode_navigation = false,
+        },
+      },
+      filetypes = { 'markdown' }, -- Filetypes to enable the plugin for
+    },
+  },
 }
