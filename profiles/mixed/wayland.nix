76941a8a1a16fda3df5a07e81cc96512e1de3cf7
Will Bush <git@willbush.dev>
2025-12-07 20:13:38 -0600

    nixos: fix eval warning

--- profiles/mixed/wayland.nix
+++ profiles/mixed/wayland.nix
@@ -42,18 +42,10 @@ in
 
     services.swayidle = {
       enable = true;
-      events = [
-        # executes command before systemd puts the computer to sleep.
-        {
-          event = "before-sleep";
-          command = lock;
-        }
-        # executes command when logind signals that the session should be locked
-        {
-          event = "lock";
-          command = lock;
-        }
-      ];
+      events = {
+        "before-sleep" = lock;
+        "lock" = lock;
+      };
       timeouts = [
         {
           timeout = 600;
