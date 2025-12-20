145d08db3d2718c87144d8b5e4f7f34fafad3e41
Will Bush <git@willbush.dev>
2025-12-20 05:42:29 -0600

    nixos: add qmk hardware support

--- profiles/nixos/host-settings.nix
+++ profiles/nixos/host-settings.nix
@@ -30,6 +30,9 @@
   security.polkit.enable = true;
   hardware.graphics.enable = true;
 
+  # enable non-root access to the firmware of QMK keyboards.
+  hardware.keyboard.qmk.enable = true;
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
