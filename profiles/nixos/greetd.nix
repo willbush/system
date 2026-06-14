c36066933afde22c45dc1d1d2c506f03bb24c0fa
Will Bush <git@willbush.dev>
2026-06-13 21:36:04 -0500

    nixos: fix greetd command format

--- profiles/nixos/greetd.nix
+++ profiles/nixos/greetd.nix
@@ -1,16 +1,18 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   services.greetd = {
     enable = true;
     settings = {
-      default_session.command = ''
-        ${pkgs.tuigreet}/bin/tuigreet \
-          --remember \
-          --time \
-          --asterisks \
-          --user-menu \
-          --cmd "uwsm start hyprland.desktop"
-      '';
+      # Build as one line. An escaped multiline string serializes to a TOML
+      # literal greetd can't parse.
+      default_session.command = lib.concatStringsSep " " [
+        "${pkgs.tuigreet}/bin/tuigreet"
+        "--remember"
+        "--time"
+        "--asterisks"
+        "--user-menu"
+        ''--cmd "uwsm start hyprland.desktop"''
+      ];
     };
   };
   # unlock keyring on login
