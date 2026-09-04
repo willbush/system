357b20b4b54216476184824d52c1a58d2ef84117
Will Bush <git@willbush.dev>
2026-09-04 08:06:25 -0500

    nixos: remove impermanence machine-id workaround
    no longer needed

--- profiles/nixos/persist.nix
+++ profiles/nixos/persist.nix
@@ -1,10 +1,5 @@
 # https://github.com/nix-community/impermanence#module-usage
 {
-  # workaround https://github.com/nix-community/impermanence/issues/229
-  # note `systemd.tmpfiles.rules` workaround did not work
-  boot.initrd.systemd.suppressedUnits = [ "systemd-machine-id-commit.service" ];
-  systemd.suppressedSystemUnits = [ "systemd-machine-id-commit.service" ];
-
   environment.persistence."/nix/persist" = {
     hideMounts = true;
     directories = [
